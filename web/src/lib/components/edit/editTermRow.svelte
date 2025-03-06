5847c9bbe48cc3408ecfdfc7268c6cdca50fed69
Ehan A <ehan@quizfreely.org>
2025-03-05 21:46:33 -0500

    something something components

---
+++ web/src/lib/components/edit/editTermRow.svelte
@@ -0,0 +1,57 @@
+<script>
+    import IconMoreDotsHorizontal from "$lib/icons/MoreDotsHorizontal.svelte"
+    import IconArrowUp from "$lib/icons/ArrowUp.svelte"
+    import IconArrowDown from "$lib/icons/ArrowDown.svelte"
+    import IconTrash from "$lib/icons/Trash.svelte"
+
+    function resizeTextarea(event) {
+        event.target.style.height = "auto";
+        event.target.style.height = (event.target.scrollHeight + 10) + "px";
+    }
+</script>
+<div class="box">
+    <input type="text" placeholder="Term" />
+    <textarea
+        class="vertical"
+        rows="2"
+        placeholder="Definition"
+        oninput={resizeTextarea}
+    >
+    </textarea>
+    <div class="flex center">
+        <div class="dropdown">
+            <button class="dropdown-toggle" aria-label="Actions dropdown menu">
+                <IconMoreDotsHorizontal />
+            </button>
+            <div class="content">
+                <button onclick={
+                    function (event) {
+                        document.getElementById("edit-terms-rows").insertBefore(
+                            document.parentElement.parentElement.parentElement.parentElement,
+                            document.parentElement.parentElement.parentElement.parentElement.previousElementSibling
+                        )
+                    }
+                }>
+                    <IconArrowUp /> Move Up
+                </button>
+                <button onclick={
+                    function (event) {
+                        document.getElementById("edit-terms-rows").insertBefore(
+                            document.parentElement.parentElement.parentElement.parentElement,
+                            document.parentElement.parentElement.parentElement.parentElement.nextElementSibling
+                        )
+                    }
+                }>
+                    <IconArrowDown /> Move Down
+                </button>
+                <button onclick={
+                    function (event) {
+                        event.target.parentElement.parentElement.parentElement.parentElement.remove()
+                    }
+                }>
+                    <IconTrash /> Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
