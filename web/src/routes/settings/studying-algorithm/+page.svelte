6f862a4bc8a1d15beaa9b9f86d90b044ed0328ed
Ehan A <ehan@quizfreely.org>
2025-04-24 16:06:39 -0400

    localStorage

--- web/src/routes/settings/studying-algorithm/+page.svelte
+++ web/src/routes/settings/studying-algorithm/+page.svelte
@@ -5,25 +5,69 @@
     import IconPencil from "$lib/icons/Pencil.svelte";
     import IconCheckmark from "$lib/icons/Checkmark.svelte";
     import IconTrash from "$lib/icons/Trash.svelte";
-
+    
     onMount(function () {
-        
+        if (window.localStorage) {
+            var goodAcc = parseInt(localStorage.getItem("quizfreely:settings.reviewMode.goodAcc"));
+            var badAcc = parseInt(localStorage.getItem("quizfreely:settings.reviewMode.badAcc"));
+            if (goodAcc > 0 && goodAcc <= 100) {
+                document.getElementById("good-acc").value = goodAcc;
+            }
+            if (badAcc > 0 && badAcc < 100) {
+                document.getElementById("bad-acc").value = badAcc;
+            }
+        }
     })
 </script>
 <style>
     .label-thingy {
-        display: block;
         margin-bottom: 0px;
     }
-    .under-label-thingy {
+    .input-thingy-container {
+        display: flex;
         margin-top: 0.4rem;
+        flex-direction: row;
+        gap: 0.4rem;
+        align-items: center;
+        align-content: center;
+    }
+    .input-thingy {
+        margin-top: 0px;
+        max-width: 5rem;
+    }
+    .input-thingy-percent {
+        margin-top: 0px;
     }
 </style>
 <div class="box">
     <h4>Review Mode</h4>
-    <p for="min-good-acc">"Good" accuracy<br><span class="fg0">Default: &gt; 90%</span></p>
-    <input type="text" id="min-good-acc" class="under-label-thingy" placeholder="90">
-    <p for="min-good-acc">"Bad" accuracy<br><span class="fg0">Default: &lt; 80%</span></p>
-    <input type="text" id="max-bad-acc" class="under-label-thingy" placeholder="80">
-    <p class="fg0">You can have different  settings for each studyset under each studyset's settings</p>
+    <p class="label-thingy">
+        "Good" accuracy<br>
+        <span class="fg0">Default: &gt; 90%</span>
+    </p>
+    <div class="input-thingy-container">
+        <input type="text" id="good-acc" class="input-thingy" placeholder="90"><span class="input-thingy-percent">%</span>
+    </div>
+    <p class="label-thingy">
+        "Bad" accuracy<br>
+        <span class="fg0">Default: &lt; 80%</span>
+    </p>
+    <div class="input-thingy-container">
+        <input type="text" id="bad-acc" class="input-thingy" placeholder="80"><span class="input-thingy-percent">%</span>
+    </div>
+    <div class="flex">
+    <button onclick={function () {
+        if (window.localStorage) {
+            localStorage.setItem(
+                "quizfreely:settings.reviewMode.goodAcc",
+                document.getElementById("good-acc").value
+            )
+            localStorage.setItem(
+                "quizfreely:settings.reviewMode.badAcc",
+                document.getElementById("bad-acc").value
+            )
+        }
+    }}><IconCheckmark /> Save</button>
+    </div>
+    <p class="fg0">You can choose different settings for each studyset under those studysets' settings.</p>
 </div>
