1c60bee9e1f6f01dabf35d3a215caed20fc12032
Ehan A <ehan@quizfreely.org>
2025-04-26 01:28:39 -0400

    ui

--- web/src/routes/ReviewModeSettings.svelte
+++ web/src/routes/ReviewModeSettings.svelte
@@ -51,6 +51,47 @@ and web/src/routes/studysets/[id]/review-mode/settings/+page.svelte
     .input-thingy-percent {
         margin-top: 0px;
     }
+
+    /* from web/src/routes/settings/+layout.svelte */
+    .settings-container {
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: 1fr 4fr;
+        grid-template-rows: 1fr;
+    }
+    .settings-menu-link {
+        margin-top: 0px;
+        color: var(--fg1);
+        padding: 0.4rem 0.8rem;
+        border-radius: 0.8rem;
+    }
+    .settings-menu-link:hover {
+        background-color: var(--bg3);
+    }
+    .settings-menu-link.current {
+        color: var(--main);
+        background-color: var(--bg3);
+    }
+    .settings-menu-link.current:hover {
+        color: var(--main-alt);
+    }
+    .settings-menu-nav {
+        display: flex;
+        flex-direction: column;
+        gap: 0.4rem;
+    }
+
+    @media only screen and (max-width: 800px) {
+        .settings-container {
+            grid-template-columns: 1fr;
+            grid-template-rows: auto 1fr;
+        }
+        .settings-menu-nav {
+            flex-direction: row;
+            gap: 1rem;
+            margin-bottom: 1rem;
+        }
+    }
 </style>
 <Noscript />
 <div class="grid page">
@@ -66,60 +107,71 @@ and web/src/routes/studysets/[id]/review-mode/settings/+page.svelte
     </a>
     {/if}
 </div>
-<div class="box">
-    <h4>Review Mode</h4>
-    <p class="label-thingy">
-        "Good" accuracy<br>
-        <span class="fg0">Default: &gt; 90%</span>
-    </p>
-    <div class="input-thingy-container">
-        <input type="text" id="good-acc" class="input-thingy" placeholder="90" oninput={() => reviewModeChangesSaved = false}>
-        <span class="input-thingy-percent">%</span>
+<div class="settings-container">
+    <div>
+        <div class="settings-menu-nav">
+        <a href="#top" class="settings-menu-link current">
+            For this studyset
+        </a>
+        <a href="/settings/studying-algorithm" class="settings-menu-link">
+            For all studysets
+        </a>
+        </div>
     </div>
-    <p class="label-thingy">
-        "Bad" accuracy<br>
-        <span class="fg0">Default: &lt; 80%</span>
-    </p>
-    <div class="input-thingy-container">
-        <input type="text" id="bad-acc" class="input-thingy" placeholder="80" oninput={() => reviewModeChangesSaved = false}>
-        <span class="input-thingy-percent">%</span>
-    </div>
-    <div class="box ohno { showInvalidReviewModeAcc ? "" : "hide" }">
-        "Good" accuracy needs to be greater than "bad" accuracy.<br>
-        Both need to be between 1 and 100
-    </div>
-    {#if reviewModeChangesSaved}
-        <p class="fg0">Changes Saved</p>
-    {/if}
-    <div class="flex">
-    <button onclick={function () {
-        if (window.localStorage) {
-            var newGoodAcc = parseFloat(document.getElementById("good-acc").value)
-            var newBadAcc = parseFloat(document.getElementById("bad-acc").value)
-            if (
-                newGoodAcc >= 1 && newGoodAcc <= 100 &&
-                newBadAcc >= 1 && newBadAcc <= 100 &&
-                newGoodAcc > newBadAcc
-            ) {
-                showInvalidReviewModeAcc = false
-                localStorage.setItem(
-                    "quizfreely:settings.reviewMode.goodAcc",
-                    newGoodAcc
-                )
-                localStorage.setItem(
-                    "quizfreely:settings.reviewMode.badAcc",
-                    newBadAcc
-                )
-                reviewModeChangesSaved = true
-                document.getElementById("good-acc").value = newGoodAcc;
-                document.getElementById("bad-acc").value = newBadAcc;
-            } else {
-                showInvalidReviewModeAcc = true
+    <div class="box" style="margin-top:0px">
+        <h4>Review Mode Settings</h4>
+        <p class="label-thingy">
+            "Good" accuracy<br>
+            <span class="fg0">Default: &gt; 90%</span>
+        </p>
+        <div class="input-thingy-container">
+            <input type="text" id="good-acc" class="input-thingy" placeholder="90" oninput={() => reviewModeChangesSaved = false}>
+            <span class="input-thingy-percent">%</span>
+        </div>
+        <p class="label-thingy">
+            "Bad" accuracy<br>
+            <span class="fg0">Default: &lt; 80%</span>
+        </p>
+        <div class="input-thingy-container">
+            <input type="text" id="bad-acc" class="input-thingy" placeholder="80" oninput={() => reviewModeChangesSaved = false}>
+            <span class="input-thingy-percent">%</span>
+        </div>
+        <div class="box ohno { showInvalidReviewModeAcc ? "" : "hide" }">
+            "Good" accuracy needs to be greater than "bad" accuracy.<br>
+            Both need to be between 1 and 100
+        </div>
+        {#if reviewModeChangesSaved}
+            <p class="fg0">Changes Saved</p>
+        {/if}
+        <div class="flex">
+        <button onclick={function () {
+            if (window.localStorage) {
+                var newGoodAcc = parseFloat(document.getElementById("good-acc").value)
+                var newBadAcc = parseFloat(document.getElementById("bad-acc").value)
+                if (
+                    newGoodAcc >= 1 && newGoodAcc <= 100 &&
+                    newBadAcc >= 1 && newBadAcc <= 100 &&
+                    newGoodAcc > newBadAcc
+                ) {
+                    showInvalidReviewModeAcc = false
+                    localStorage.setItem(
+                        "quizfreely:settings.reviewMode.goodAcc",
+                        newGoodAcc
+                    )
+                    localStorage.setItem(
+                        "quizfreely:settings.reviewMode.badAcc",
+                        newBadAcc
+                    )
+                    reviewModeChangesSaved = true
+                    document.getElementById("good-acc").value = newGoodAcc;
+                    document.getElementById("bad-acc").value = newBadAcc;
+                } else {
+                    showInvalidReviewModeAcc = true
+                }
             }
-        }
-    }}><IconCheckmark /> Save</button>
+        }}><IconCheckmark /> Save</button>
+        </div>
     </div>
-    <p class="fg0">You can choose different settings for each studyset inside Review Mode for that studyset.</p>
 </div>
 </div>
 </div>
