3104d49bd40cd0e542a7dfea06f141769ad20671
Ehan <ehanahamed@ehan.dev>
2025-04-30 18:01:11 -0400

    progress bar + fix allowed link regexes for searchparam parse

--- web/src/lib/safelyParseLinkSearchParam.js
+++ web/src/lib/safelyParseLinkSearchParam.js
@@ -2,8 +2,8 @@ export default function(searchParams, searchParamName) {
     let link;
 	let param = searchParams.get(searchParamName)
 	let allowedLinkRegexes = [
-		/^\/studysets\/[a-z0-9-]+\/review-mode\/settings$/,
-		/^\/studyset\/local\/review-mode\/settings\?id=[0-9]+$/
+		/^\/studysets\/[a-z0-9-]+\/settings$/,
+		/^\/studyset\/local\/settings\?id=[0-9]+$/
 	]
 	if (param != null) {
 		var allowed = allowedLinkRegexes.some(
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 import Header from "$lib/components/Header.svelte";
 import Footer from "$lib/components/Footer.svelte";
 import { fade } from "svelte/transition";
+import { SvelteKitTopLoader } from "sveltekit-top-loader";
 let { children, data } = $props();
 </script>
 
+<SvelteKitTopLoader color="var(--main)" showSpinner={false}/>
 <Header />
 {#key data.transPageKey}
 <div in:fade={{ duration: 140, delay: 140 }} out:fade={{ duration: 140 }}>
