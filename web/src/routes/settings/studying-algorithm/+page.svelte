0993c1c97f749b496c288dfad5176f3d3006dac7
Ehan A <ehan@quizfreely.org>
2025-05-04 19:31:22 -0400

    update other settings page too

--- web/src/routes/settings/studying-algorithm/+page.svelte
+++ web/src/routes/settings/studying-algorithm/+page.svelte
@@ -5,18 +5,24 @@
     import IconPencil from "$lib/icons/Pencil.svelte";
     import IconCheckmark from "$lib/icons/Checkmark.svelte";
     import IconTrash from "$lib/icons/Trash.svelte";
+    import Local from "$lib/icons/Local.svelte";
     var showInvalidAcc = $state(false);
+    var showInvalidLearningMinSessionsCount = $state(false);
     var changesSaved = $state(false);
     
     onMount(function () {
         if (window.localStorage) {
             var goodAcc = parseFloat(localStorage.getItem("quizfreely:settings.studyingAlgorithm.goodAcc"));
             var badAcc = parseFloat(localStorage.getItem("quizfreely:settings.studyingAlgorithm.badAcc"));
+            var learningMinSessionsCount = parseFloat(localStorage.getItem("quizfreely:settings.studyingAlgorithm.learningMinSessionsCount"));
             if (goodAcc >= 1 && goodAcc <= 100) {
                 document.getElementById("studying-algorithm-good-acc").value = goodAcc;
             }
             if (badAcc >= 0 && badAcc <= 100) {
                 document.getElementById("studying-algorithm-bad-acc").value = badAcc;
+            }
+            if (learningMinSessionsCount >= 1) {
+              document.getElementById("learning-min-sessions-count").value = learningMinSessionsCount;
             }
         }
     })
@@ -42,6 +48,8 @@
     }
 </style>
 <div class="box">
+    <div class="flex" style="gap: 2rem;">
+    <div>
     <p class="label-thingy">
         "Good" accuracy<br>
         <span class="fg0">Default: &gt; 90%</span>
@@ -50,6 +58,8 @@
         <input type="text" id="studying-algorithm-good-acc" class="input-thingy" placeholder="90" oninput={() => changesSaved = false}>
         <span class="input-thingy-percent">%</span>
     </div>
+    </div>
+    <div>
     <p class="label-thingy">
         "Bad" accuracy<br>
         <span class="fg0">Default: &lt; 80%</span>
@@ -58,10 +68,23 @@
         <input type="text" id="studying-algorithm-bad-acc" class="input-thingy" placeholder="80" oninput={() => changesSaved = false}>
         <span class="input-thingy-percent">%</span>
     </div>
+    </div>
+    </div>
+    <p class="label-thingy">
+            Minimum review count for new(ish) terms<br>
+            <span class="fg0">Default: 5</span>
+        </p>
+        <div class="input-thingy-container">
+            <input type="text" id="studying-algorithm-learning-min-sessions-count" class="input-thingy" placeholder="5" oninput={() => changesSaved = false}>
+        </div>
     <div class="box ohno { showInvalidAcc ? "" : "hide" }">
         "Good" accuracy needs to be greater than "bad" accuracy.<br>
         Both need to be between 1 and 100
     </div>
+            <div class="box ohno { showInvalidLearningMinSessionsCount ? "" : "hide" }">
+            Min review count needs to be a number greater than or equal to 1
+        </div>
+
     {#if changesSaved}
         <p class="fg0">Changes Saved</p>
     {/if}
@@ -70,6 +93,7 @@
         if (window.localStorage) {
             var newGoodAcc = parseFloat(document.getElementById("studying-algorithm-good-acc").value)
             var newBadAcc = parseFloat(document.getElementById("studying-algorithm-bad-acc").value)
+            var newLearningMinSessionsCount = parseFloat(document.getElementById("studying-algorithm-learning-min-sessions-count").value)
 
             showInvalidAcc = false;
 
@@ -107,7 +131,17 @@
                 showInvalidAcc = true;
             }
 
-            if (!showInvalidAcc) {
+            if (newLearningMinSessionsCount >= 1) {
+                localStorage.setItem("quizfreely:settings.studyingAlgorithm.learningMinSessionsCount",
+                    newLearningMinSessionsCount
+                )
+            } else if (document.getElementById("studying-algorithm-learning-min-sessions-count").value) {
+                localStorage.removeItem("quizfreely:settings.studyingAlgorithm.learningMinSessionsCount")
+            } else {
+                showInvalidLearningMinSessionsCount = true;
+            }
+
+            if ((!showInvalidAcc) && (!showInvalidLearningMinSessionsCount)) {
                 changesSaved = true;
             }
         }
