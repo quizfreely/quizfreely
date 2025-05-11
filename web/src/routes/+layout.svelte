6f93b77511a3991d0b536605a09f45d5e4fa3d3a
Ehan <ehanahamed@ehan.dev>
2025-05-11 16:58:00 -0400

    Switch to real nprogress

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,11 +2,22 @@
 import Header from "$lib/components/Header.svelte";
 import Footer from "$lib/components/Footer.svelte";
 import { fade } from "svelte/transition";
-import { SvelteKitTopLoader } from "sveltekit-top-loader";
+import NProgress from "nprogress";
+import "$lib/nprogress/modified-nprogress.css";
+import { beforeNavigate, afterNavigate } from "$app/navigation";
 let { children, data } = $props();
+
+NProgress.configure({
+    showSpinner: false
+});
+beforeNavigate(function () {
+    NProgress.start();
+})
+afterNavigate(function () {
+    NProgress.done();
+})
 </script>
 
-<SvelteKitTopLoader color="var(--main)" showSpinner={false}/>
 <Header />
 {#key data.transPageKey}
 <div in:fade={{ duration: 140, delay: 140 }} out:fade={{ duration: 140 }}>
@@ -16,3 +27,4 @@ let { children, data } = $props();
     <Footer />
 </div>
 {/key}
+
