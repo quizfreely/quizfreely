0fd67462d4bfe8c1676cadf159357e0e954ee993
Ehan A <ehan@quizfreely.org>
2025-05-18 01:42:16 -0400

    yay

--- web/src/routes/classes/ClassesDashboard.svelte
+++ web/src/routes/classes/ClassesDashboard.svelte
@@ -11,6 +11,24 @@
 <main>
     <div class="grid page">
         <div class="content">
+            {#if data?.classesData?.classesAsTeacher?.length > 0}
+                <div class="grid list">
+                    {#each data.classesData.classesAsTeacher as classobj}
+                        <div class="box">
+                            <p>{classobj.name}</p>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
+            {#if data?.classesData?.classesAsStudent?.length > 0}
+                <div class="grid list">
+                    {#each data.classesData.classesAsStudent as classobj}
+                        <div class="box">
+                            <p>{classobj.name}</p>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
             {JSON.stringify(
                 data.classesData
             )}
