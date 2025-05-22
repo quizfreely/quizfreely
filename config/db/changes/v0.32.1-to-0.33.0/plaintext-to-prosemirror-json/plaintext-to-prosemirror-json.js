431c2b086efa8b0c27dbc09c3d959ac98af178e9
Ehan A <ehan@quizfreely.org>
2025-05-22 17:12:18 -0400

    devops?

--- config/db/changes/v0.32.1-to-0.33.0/plaintext-to-prosemirror-json/plaintext-to-prosemirror-json.js
+++ config/db/changes/v0.32.1-to-0.33.0/plaintext-to-prosemirror-json/plaintext-to-prosemirror-json.js
@@ -2,7 +2,7 @@ import { JSDOM } from "jsdom";
 import { DOMParser as PMDOMParser } from "prosemirror-model";
 import { schema as basicSchema } from "prosemirror-schema-basic";
 
-function convertPlaintextToPMJsonServer(plaintext) {
+function convertPlaintextToProsemirrorJson(plaintext) {
   const dom = new JSDOM("<div></div>");
   const div = dom.window.document.querySelector("div");
   div.innerHTML = plaintext.split(/\n+/).map(
@@ -12,4 +12,6 @@ function convertPlaintextToPMJsonServer(plaintext) {
   const doc = PMDOMParser.fromSchema(basicSchema).parse(div);
   return doc.toJSON();
 }
+
+
 
