1df273adf13471894807391ecdf7f7177e0a6af8
Ehan <ehanahamed@ehan.dev>
2025-05-27 01:02:17 -0400

    sleepy

--- web/src/routes/classes/ClassesDashboard.svelte
+++ web/src/routes/classes/ClassesDashboard.svelte
@@ -14,6 +14,24 @@
 .class-link:hover {
     color: var(--fg0);
 }
+.split-style-container-thing {
+    display: grid;
+    gap: 0px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+}
+.split-style-container-thing .left-div {
+    justify-self: end;
+    margin-top: 0px;
+    padding: 1rem;
+    border-right: 0.1rem solid var(--bg4);
+}
+.split-style-container-thing .right-div {
+    justify-self: start;
+    margin-top: 0px;
+    padding: 1rem;
+    border-left: 0.1rem solid var(--bg4);
+}
 </style>
 
 <svelte:head>
@@ -42,8 +60,15 @@
                     {/each}
                 </div>
             {/if}
-            {#if !(data?.classesData?.classesAsStudent?.length > 0)}
-                a
+            {#if !(data?.classesData?.classesAsStudent?.length > 0 || data?.classesData?.classesAsTeacher?.length > 0)}
+                <div class="split-style-container-thing">
+                    <div class="left-div">
+                        <h2 class="h3">Students</h2>
+                    </div>
+                    <div class="right-div">
+                        <h2 class="h3">Teachers</h2>
+                    </div>
+                </div>
             {/if}
             <!-- <p style="white-space: pre"> -->
             <!-- {JSON.stringify( -->
