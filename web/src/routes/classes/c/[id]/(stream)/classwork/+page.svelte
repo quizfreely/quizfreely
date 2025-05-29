aa88f0ed9c9bf6550176963d9387188ff6063660
Ehan <ehanahamed@ehan.dev>
2025-05-29 04:52:23 -0400

    adfjkl

--- web/src/routes/classes/c/[id]/(stream)/classwork/+page.svelte
+++ web/src/routes/classes/c/[id]/(stream)/classwork/+page.svelte
@@ -41,45 +41,21 @@
     {#if data?.classData?.classById?.assignmentDrafts?.length > 0}
     Drafts <span class="fg0">(not visible to students)</span>
     {#each data?.classData?.classById?.assignmentDrafts as draft}
-        <div class="box announcement">
-            <p style="font-size: 1.2rem;">{draft.title}</p>
-            <div class="flex" style="align-items: center; margin-top: 0.4rem;">
-                <a href="/classes/c/{data.classId}/edit-assignment-draft/{draft.id}" class="button faint" style="margin-top: 0px;">
-                    <IconPencil></IconPencil>
-                    Edit
-                </a>
-            {#if draft.renderedDueDate}
-                <span class="fg0" style="margin-left: auto; margin-right: 1rem;">Due {draft.renderedDueDate}</span>
-            {/if}
-            </div>
-        </div>
+        <AssignmentsListItem title={draft.title} renderedDueDate={draft.renderedDueDate} amIATeacher hideViewButton="true" trustedEditHref="/classes/c/{ data.classId }/edit-assignment-drafts/{ draft.id }" descriptionSafeHtml={draft.safeRenderedHtml}></AssignmentsListItem>
     {/each}
     {/if}
     {#if data?.classData?.classById?.assignments?.length > 0}
-    Assignments
+    <p>Assignments</p>
     {#each data?.classData?.classById?.assignments as assignment}
-        <AssignmentsListItem title={assignment.title} renderedDueDate={assignment.renderedDueDate} trustedViewHref="/classes/c/{ data.classId }/assignments/{ assignment.id }" trustedEditHref="/classes/c/{ data.classId }/edit-assignments/{ assignment.id }" descriptionSafeHtml={assignment.safeRenderedHtml}></AssignmentsListItem>
-        <div class="box announcement">
-            <p style="font-size: 1.2rem;">{assignment.title}</p>
-            <div class="flex" style="align-items: center; margin-top: 0.4rem;">
-                <a href="/classes/c/{data.classId}/edit-assignment/{assignment.id}" class="button faint" style="margin-top: 0px;">
-                    <IconPencil></IconPencil>
-                    Edit
-                </a>
-            {#if assignment.renderedDueDate}
-                <span class="fg0" style="margin-left: auto; margin-right: 1rem;">Due {assignment.renderedDueDate}</span>
-            {/if}
-            </div>
-        </div>
+        <AssignmentsListItem title={assignment.title} renderedDueDate={assignment.renderedDueDate} amIATeacher trustedViewHref="/classes/c/{ data.classId }/assignments/{ assignment.id }" trustedEditHref="/classes/c/{ data.classId }/edit-assignments/{ assignment.id }" descriptionSafeHtml={assignment.safeRenderedHtml}></AssignmentsListItem>
     {/each}
     {/if}
-    <AssignmentsListItem title="title goes here" amIATeacher editHref="/a" viewHref="/b" ></AssignmentsListItem>
-    <p style="white-space: pre-wrap;">
-    {JSON.stringify(
-        data.classData,
-        null,
-        4
-    )}
-    </p>
+    <!-- <p style="white-space: pre-wrap;"> -->
+    <!-- {JSON.stringify( -->
+    <!--     data.classData, -->
+    <!--     null, -->
+    <!--     4 -->
+    <!-- )} -->
+    <!-- </p> -->
 </div>
 
