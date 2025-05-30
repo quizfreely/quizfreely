284842b54c895cbf792e0a75e91d7052bf98dfff
Ehan <ehanahamed@ehan.dev>
2025-05-30 15:31:31 +0000

    calendar

---
+++ web/src/routes/classes/calendar/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {Calendar, TimeGrid} from '@event-calendar/core';
+
+    let options = $state({
+        view: 'timeGridWeek',
+        events: [
+            // your list of events
+        ]
+    });
+</script>
+
+<Calendar plugins={[TimeGrid]} {options} />
