4667f6714cdc958bfe19a92eb366a1c624658573
Ehan A <ehan@quizfreely.org>
2025-05-31 17:54:45 -0400

    ui?

--- web/src/routes/classes/calendar/+page.svelte
+++ web/src/routes/classes/calendar/+page.svelte
@@ -1,20 +1,54 @@
 <script>
-    import {Calendar, TimeGrid} from '@event-calendar/core';
+    import {Calendar, DayGrid, List, TimeGrid} from '@event-calendar/core';
     let { data } = $props();
 
     let options = $state({
         view: 'timeGridWeek',
         events: [
             // your list of events
-        ]
+        ],
+        headerToolbar: {
+            start: 'prev,next today',
+            center: 'title',
+            end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        buttonText: {
+            close: 'Close',
+            dayGridMonth: 'Month',
+            listDay: 'List',
+            listMonth: 'List',
+            listWeek: 'List',
+            listYear: 'List',
+            resourceTimeGridDay: 'resources',
+            resourceTimeGridWeek: 'resources',
+            resourceTimelineDay: 'timeline',
+            resourceTimelineMonth: 'timeline',
+            resourceTimelineWeek: 'timeline',
+            timeGridDay: 'Day',
+            timeGridWeek: 'Week',
+            today: 'Today'
+        }
     });
 </script>
-<div class={
+<style>
+    :global {
+        .thecontainer {
+            max-width: 80vw;
+        }
+        .ec-title {
+            font-size: 1.6rem;
+            font-weight: normal;
+        }
+    }
+</style>
+<div class="grid page">
+    <div class="content thecontainer no-child-margin-top {
     data.theme == "auto" ?
         "ec-auto-dark" :
         data.theme == "dark" ?
             "ec-dark" :
             ""
-}>
-<Calendar plugins={[TimeGrid]} {options} />
+}">
+<Calendar plugins={[DayGrid, List, TimeGrid]} {options} />
+    </div>
 </div>
