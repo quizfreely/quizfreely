efea2c0af5a9be547cea8af1f6fb31484d1ad0ed
Ehan <ehanahamed@ehan.dev>
2025-06-02 01:16:56 -0400

    layout for view

--- web/src/routes/classes/c/[id]/assignments/[assignmentid]/+page.svelte
+++ web/src/routes/classes/c/[id]/assignments/[assignmentid]/+page.svelte
@@ -17,7 +17,7 @@
     .assignment-container-grid {
         display: grid;
         gap: 1rem;
-        grid-template-columns: 4fr 1fr;
+        grid-template-columns: auto 14rem;
         grid-template-rows: auto;
         grid-template-areas: "details work";
     }
@@ -44,13 +44,13 @@
               <IconBackArrow /> Back
             </a>
         </div>
+        <h1 class="h3">{data?.classData?.assignmentById?.title}</h1>
         <div class="assignment-container-grid">
             <div class="assignment-details">
-                <h1 class="h3">{data?.classData?.assignmentById?.title}</h1>
                 <div class="description-container no-child-margin-top">{@html data?.classData?.assignmentById?.safeRenderedHtml}</div>
             </div>
-            <div class="assignment-work">
-                a
+            <div class="assignment-work no-margin-top">
+                <button class="fullWidth">Turn in</button>
             </div>
         </div>
     </div>
