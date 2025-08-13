a9d54b4d5387325f81070bbc6dd307204c9e9c1a
Ehan A <ehan@quizfreely.org>
2025-08-12 22:58:17 -0400

    fix auth err on landing page

--- web/src/routes/landing-page/+page.server.js
+++ web/src/routes/landing-page/+page.server.js
@@ -1,14 +1,24 @@
 import { env } from '$env/dynamic/private';
 
-export async function load() {
+export async function load({ cookies }) {
+    const authToken = cookies.get("auth");
   try {
   let response = await fetch(env.API_URL + "/graphql", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Authorization": authToken != null ?
+        `Bearer ${authToken}` :
+        undefined
     },
     body: JSON.stringify({
       query: `query {
+        authed
+        authedUser {
+          id
+          username
+          display_name
+        }
         featuredStudysets {
           id
           title
@@ -24,24 +34,28 @@ export async function load() {
   let responseJson = await response.json()
       if (responseJson?.data?.featuredStudysets?.length >= 0) {
         return {
+          authed: responseJson?.data?.authed,
+          authedUser: responseJson?.data?.authedUser,
           featuredRows: responseJson.data.featuredStudysets,
-      header: { activePage: "home" }
+          header: { activePage: "home" }
         };
       } else {
         return {
+          authed: responseJson?.data?.authed,
+          authedUser: responseJson?.data?.authedUser,
           featuredRows: false,
       header: { activePage: "home" }
         }
       }
-    } catch (error) {
-      //request.log.error(error);
+    } catch (err) {
+      console.error(err);
       return {
         featuredRows: false,
       header: { activePage: "home" }
       }
     };
-  } catch (error) {
-    //request.log.error(error);
+  } catch (err) {
+    console.error(err);
     return {
       featuredRows: false,
       header: { activePage: "home" }
