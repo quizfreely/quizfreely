9a5bf9ac732bd643b7211aea7b48c55c2f9dedb9
Ehan A <ehan@quizfreely.org>
2025-08-16 17:55:45 +0000

    Update quizfreely/idb-api-layer

---
+++ web/src/lib/idb-api-layer/db.js
@@ -0,0 +1,51 @@
+/*!
+ * Quizfreely IDB API Layer, licensed under GPL-3.0-or-later.
+ * Copyright (c) 2025 Ehan Ahamed and contributors
+ *
+ * https://codeberg.org/quizfreely/idb-api-layer
+ * https://github.com/quizfreely/idb-api-layer
+ */
+
+import Dexie from 'dexie';
+
+const db = new Dexie("quizfreelydata");
+db.version(5).stores({
+    studysets: '++id, title',
+    studysetprogress: 'studyset_id',
+    studysetsettings: 'studyset_id'
+});
+db.version(6).stores({
+    studysets: '++id, updated_at',
+    terms: "++id, studyset_id, sort_order, created_at, updated_at",
+    term_progress: "++id, term_id, term_first_reviewed_at, term_last_reviewed_at, " +
+        "term_review_count, def_first_reviewed_at, def_last_reviewed_at, " +
+        "def_review_count, term_leitner_system_box, def_leitner_system_box",
+    studysetprogress: null,
+    studysetsettings: null
+}).upgrade(async (tx) => {
+    const oldStudysets = await tx.table("studysets").toArray();
+    for (const studyset of oldStudysets) {
+        if (studyset?.data?.terms != null && studyset.data.terms.length > 0) {
+            let sortOrder = 0;
+
+            for (const [term, def] of studyset.data.terms) {
+                await tx.table("terms").add({
+                    term: term,
+                    def: def,
+                    studyset_id: studyset.id,
+                    sort_order: sortOrder,
+                    created_at: studyset.updated_at,
+                    updated_at: studyset.updated_at
+                });
+
+                sortOrder++;
+            }
+
+            delete studyset.data;
+
+            await tx.table("studysets").put(studyset);
+        }
+    }
+})
+
+export default db;
---
+++ web/src/lib/idb-api-layer/idb-api-layer.js
@@ -0,0 +1,11 @@
+/*!
+ * Quizfreely IDB API Layer, licensed under GPL-3.0-or-later.
+ * Copyright (c) 2025 Ehan Ahamed and contributors
+ *
+ * https://codeberg.org/quizfreely/idb-api-layer
+ * https://github.com/quizfreely/idb-api-layer
+ */
+
+export const idbApiLayer = {
+    
+}
