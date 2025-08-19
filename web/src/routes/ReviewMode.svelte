561b52aa3b1bfa22a96781f4d5f9d19fb1dd2770
Ehan A <ehan@quizfreely.org>
2025-08-19 00:12:02 -0400

    idk

--- web/src/routes/ReviewMode.svelte
+++ web/src/routes/ReviewMode.svelte
@@ -1,4 +1,10 @@
 <script>
     import idbApiLayer from "$lib/idb-api-layer/idb-api-layer.js";
+    let data = $props();
+    
 </script>
-<h4></h4>
+<div class="grid page">
+    <div class="content">
+        <h4>Overall Accuracy: </h4>
+    </div>
+</div>
