ca0e58fe15f0e8116c82bae92a369618a90a9691
Ehan A <ehan@quizfreely.org>
2025-08-20 02:33:49 -0400

    debug

--- web/src/routes/PracticeTest.svelte
+++ web/src/routes/PracticeTest.svelte
@@ -1,3 +1,16 @@
 <script>
+    import idbApiLayer from "$lib/idb-api-layer/idb-api-layer.js";
     let { data } = $props();
+    let terms = $state();
+    if (data.local) {
+        (async () => {
+            const studyset = await idbApiLayer.getStudysetById(data.localId, {
+                terms: {
+                    progress: true
+                }
+            })
+            terms = studyset.terms;
+        })();
+    }
 </script>
+<p>{JSON.stringify(terms, undefined, 4)}</p>
