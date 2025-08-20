b2577374a636fa150594aedfa854fdda03626736
Ehan A <ehan@quizfreely.org>
2025-08-20 12:39:37 -0400

    continue working on ui

--- web/src/routes/PracticeTest.svelte
+++ web/src/routes/PracticeTest.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import idbApiLayer from "$lib/idb-api-layer/idb-api-layer.js";
     import BackIcon from "$lib/icons/BackArrow.svelte";
+    import CheckmarkIcon from "$lib/icons/Checkmark.svelte";
     let { data } = $props();
     let terms = $state();
 
@@ -44,7 +45,37 @@
                 `/studysets/${data.studysetId}`
             }><BackIcon></BackIcon> Back</a>
         </div>
-        <h1></h1>
+        <p class="h3" style="margin-top: 1rem;">Practice Test</p>
+        <p>There are {terms?.length} total terms</p>
+        <div class="flex" style="align-content: center; align-items: center;">
+            <span>Questions:</span>
+            <input type="text" placeholder={terms?.length} style="max-width: 4rem;">
+        </div>
+        <p>Answer with:</p>
+        <div class="flex">
+            <button class="button-box selected">Definition</button>
+            <button class="button-box"> Term</button>
+            <button class="button-box">Both</button>
+        </div>
+        <p>Question types:</p>
+        <div style="display: grid; grid-template-columns: auto; justify-content: start;">
+            <button class="button-box selected" style="display: flex;">
+                <CheckmarkIcon></CheckmarkIcon>
+                Multiple Choice
+            </button>
+            <button class="button-box selected" style="display: flex;">
+                <CheckmarkIcon></CheckmarkIcon>
+                True/False
+            </button>
+            <button class="button-box selected" style="display: flex;">
+                <CheckmarkIcon></CheckmarkIcon>
+                Matching
+            </button>
+            <button class="button-box selected" style="display: flex;">
+                <CheckmarkIcon></CheckmarkIcon>
+                Free Response
+            </button>
+        </div>
         <p style="white-space: pre-wrap">{JSON.stringify(terms, null, 4)}</p>
         {#each terms as term}
             {#if newEachRandom() < 0.5}
