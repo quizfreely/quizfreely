d271001acd48534dccdd56ffa2164c3945427b08
Ehan A <ehan@quizfreely.org>
2025-08-31 17:32:35 -0400

    asdf

---
+++ web/src/routes/studysets/[id]/stats/+page.server.js
@@ -0,0 +1,93 @@
+import { env } from "$env/dynamic/private";
+
+export async function load({ cookies, params}) {
+    try {
+        const respRaw = await fetch(env.API_URL + "/graphql", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${cookies.get("auth")}`
+            },
+            body: JSON.stringify({
+                query: `query studysetStats($studysetId: ID!) {
+    authed
+    authedUser {
+        id
+        username
+        displayName
+    }
+    studyset(id: $studysetId) {
+        id
+        title
+        terms {
+            id
+            term
+            def
+            progress {
+                termFirstReviewedAt
+                termLastReviewedAt
+                termReviewCount
+                defFirstReviewedAt
+                defLastReviewedAt
+                defReviewCount
+                termCorrectCount
+                termIncorrectCount
+                defCorrectCount
+                defIncorrectCount
+            }
+            topConfusionPairs {
+                confusedTerm {
+                    id
+                    term
+                    def
+                }
+                answeredWith
+                confusedCount
+            }
+            topReverseConfusionPairs {
+                term {
+                    id
+                    term
+                    def
+                }
+                answeredWith
+                confusedCount
+            }
+        }
+        practiceTests {
+            id
+            timestamp
+            questionsCorrect
+            questionsTotal
+        }
+    }
+}`,
+                variables: {
+                    studysetId: params.id
+                }
+            })
+        });
+        const resp = await respRaw.json();
+        if (resp?.data == null || resp?.errors != null) {
+            console.log("Error in cloud studyset stats load func api request. Response: ", resp);
+        }
+        return {
+            studysetId: params.id,
+            studyset: resp?.data?.studyset,
+            authed: resp?.data?.authed,
+            authedUser: resp?.data?.authedUser,
+            settingsDateTimeFmtHours: cookies.get(
+              "settingsdatetimeformathours"
+            )
+        }
+    } catch (err) {
+        console.error("Error in cloud studyset stats load func: ", err);
+        return {
+          studysetId: params.id,
+          authed: false,
+          settingsDateTimeFmtHours: cookies.get(
+            "settingsdatetimeformathours"
+          )
+        }
+    }
+}
--- web/src/routes/studysets/[id]/stats/+page.svelte
+++ web/src/routes/studysets/[id]/stats/+page.svelte
@@ -1,23 +1,6 @@
 <script>
-    import { onMount } from "svelte";
-    import Chart from 'chart.js/auto';
-    import { setEhuiChartColors } from "$lib/ehui-chartjs-colors.js";
-    let chartCanvas;
-    onMount(() => {
-        setEhuiChartColors(Chart);
-        // new Chart(
-        //     chartCanvas,
-        //     {
-        //         type: "line",
-        //         data: {
-        //             labels: ["a", "b", "c"],
-        //             dataset: {
-        //                 fill: false,
-        //                 tension: 0
-        //             }
-        //         }
-        //     }
-        // )
-    })
+    import StudysetStats from "../../../StudysetStats.svelte"
+    let { data } = $props();
 </script>
-<canvas bind:this={chartCanvas}></canvas>
+
+<StudysetStats data={{ ...data, local: false }} />
