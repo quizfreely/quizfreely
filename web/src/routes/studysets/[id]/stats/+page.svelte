30acbda3297282a3c4c8167d5423de53c2c5be94
Ehan A <ehan@quizfreely.org>
2025-08-31 17:25:03 -0400

    stats page

---
+++ web/src/routes/studysets/[id]/stats/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+    import Chart from 'chart.js/auto';
+    import { setEhuiChartColors } from "$lib/ehui-chartjs-colors.js";
+    let chartCanvas;
+    onMount(() => {
+        setEhuiChartColors(Chart);
+        // new Chart(
+        //     chartCanvas,
+        //     {
+        //         type: "line",
+        //         data: {
+        //             labels: ["a", "b", "c"],
+        //             dataset: {
+        //                 fill: false,
+        //                 tension: 0
+        //             }
+        //         }
+        //     }
+        // )
+    })
+</script>
+<canvas bind:this={chartCanvas}></canvas>
