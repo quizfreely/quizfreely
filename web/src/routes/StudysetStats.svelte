b60f565b506be20b966439e87a100b7bd911651e
Ehan A <ehan@quizfreely.org>
2025-09-01 00:17:35 -0400

    asdf

--- web/src/routes/StudysetStats.svelte
+++ web/src/routes/StudysetStats.svelte
@@ -1,23 +1,106 @@
 <script>
     import { onMount } from "svelte";
     import Chart from 'chart.js/auto';
+    import 'chartjs-adapter-luxon';
     import { setEhuiChartColors } from "$lib/ehui-chartjs-colors.js";
+    let { data } = $props();
+    let terms = $state();
+    let practiceTests = $state([]);
+    const DISPLAY_TOP_N_PRACTICE_TESTS = 4;
+
+    if (!data.local) {
+        console.log(data.studyset)
+        terms = data?.studyset?.terms;
+        practiceTests = data?.studyset?.practiceTests?.slice(0, DISPLAY_TOP_N_PRACTICE_TESTS);
+    }
+
     let chartCanvas;
+
+    let mounted = $state(false);
     onMount(() => {
+        mounted = true;
+        if (data?.settingsDateTimeFmtHours == "24") {
+            fancyTimestamp.hours = 24;
+        } else if (data?.settingsDateTimeFmtHours == "12") {
+            fancyTimestamp.hours = 12;
+        }
+
+        if (data.local) {
+            /* studyset is local, so regardless of wheater the user is logged in or not,
+            we load the studyset and progress locally */
+            (async () => {
+                const studyset = await idbApiLayer.getStudysetById(data.localId, {
+                    terms: {
+                        progress: true,
+                        topConfusionPairs: {
+                            confusedTerm: true
+                        },
+                        topReverseConfusionPairs: {
+                            term: true
+                        }
+                    },
+                    practiceTests: true
+                })
+                terms = studyset.terms;
+                practiceTests = studyset?.practiceTests?.slice(0, DISPLAY_TOP_N_PRACTICE_TESTS);
+            })();
+        }
+
+        if (!data.authed && !data.local) {
+            /* not logged in, so user data is local,
+            but studyset is a cloud studyset,
+            so we need to map local progress to cloud terms
+
+            `terms` has already been populated during SSR (above, before onMount) */
+            (async () => {
+                practiceTests = await db.practiceTests.where("studysetId").equals(data.studysetId).toArray();
+                practiceTests?.sort(
+                    /* timestamps are ISO strings in UTC,
+                    so lexical/alphanumeric sorting is the same as chronological sorting
+                    also you see we're comparing `b` to `a`, so its descending,
+                    so most recent is first */
+                    (a, b) => b.timestamp.localeCompare(a.timestamp)
+                );
+                practiceTests = practiceTests?.slice(0, DISPLAY_TOP_N_PRACTICE_TESTS);
+
+                for (const term of terms) {
+                    term.progress = await db.termProgress.where("termId").equals(term.id).toArray()?.[0];
+                    term.topConfusionPairs = await idbApiLayer.getTopConfusionPairs(term.id)
+                    term.topReverseConfusionPairs = await idbApiLayer.getTopReverseConfusionPairs(term.id)
+                }
+            })();
+        }
+
         setEhuiChartColors(Chart);
-        // new Chart(
-        //     chartCanvas,
-        //     {
-        //         type: "line",
-        //         data: {
-        //             labels: ["a", "b", "c"],
-        //             dataset: {
-        //                 fill: false,
-        //                 tension: 0
-        //             }
-        //         }
-        //     }
-        // )
+        if (practiceTests?.length > 1) {
+            new Chart(
+                chartCanvas,
+                {
+                    type: "line",
+                    data: {
+                        datasets: [
+                            ...(practiceTests?.length > 1 ? [{
+                                fill: false,
+                                tension: 0,
+                                data: practiceTests.map(pt => ({
+                                    x: Date.parse(pt.timestamp),
+                                    y: Math.floor((pt.questionsCorrect / pt.questionsTotal) * 100)
+                                }))
+                            }] : [])
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                type: "time"
+                            }
+                        }
+                    }
+                }
+            )
+        }
     })
 </script>
+{#if practiceTests?.length > 1}
 <canvas bind:this={chartCanvas}></canvas>
+{/if}
