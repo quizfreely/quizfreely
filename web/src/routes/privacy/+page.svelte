1348487ef5634c039424180fefcf152e342c0bb6
Ehan A <ehan@quizfreely.org>
2025-09-13 23:56:43 -0400

    update privacy policy

--- web/src/routes/privacy/+page.svelte
+++ web/src/routes/privacy/+page.svelte
@@ -1,29 +1,46 @@
+<style>
+    .nice-paragraph-size {
+        max-width: 40rem;
+    }
+</style>
 <svelte:head>
     <title>Quizfreely Privacy Policy</title>
     <meta name="description" content="Quizfreely is a free studying app that respects your privacy." />
 </svelte:head>
-
 <main>
   <div class="grid page">
     <div class="content">
       <h2>Privacy Policy</h2>
-      <p>Updated September 15, 2024 (2024-09-15)</p>
-      <p>
-        Quizfreely only stores information or data from users with a Quizfreely account.<br />
-        Users who use Quizfreely without an account have their data stored on their own devices.<br />
-        Quizfreely does not collect or store any data from users that do not have an account.
+      <p class="fg0">Updated 13 September 2025 (2025-09-13)</p>
+      <p class="nice-paragraph-size">
+        This privacy policy describes how data is collected and used when users use Quizfreely's website, quizfreely.org.
       </p>
-      <p>
-        When users create a Quizfreely account, Quizfreely stores their username, display name, and encrypted password to let them log in to and use their account.<br />
-        When users create a Quizfreely account through Google, Quizfreely stores their Google account id and email address to let them log in to their Quizfreely account through Google.
+      <p class="nice-paragraph-size">
+        Quizfreely ONLY collects data from users who sign up for an account on quizfreely.org.
+        Users who use quizfreely.org without an account have all the data they enter stored directly on their own devices, without being collected by Quizfreely.
       </p>
-      <p>
-        When users create or interact with content on Quizfreely while logged in to a Quizfreely account, Quizfreely stores content that they post.<br />
-        Quizfreely lets other users view and interact with public content that users post.
+      <p class="nice-paragraph-size">
+        When users create an account on quizfreely.org, their username, display name, and encrypted password are collected to let them log in and use their account.
+        If users sign in to quizfreely.org through Google, Quizfreely collects the id and email address associated with their Google account to let them log in to quizfreely.org through Google. Users' data on quizfreely.org is NOT disclosed to, shared with, or sold to Google.
       </p>
-      <p>
-        Quizfreely does NOT disclose or sell any user information or data to any other third party or company.<br />
-        Even when users log in to their Quizfreely account through Google, which is a third party, Quizfreely does not disclose or give any user data to Google.
+      <p class="nice-paragraph-size">
+        When users with an account create flashcard sets, called studysets, or comments on quizfreely.org, Quizfreely collects the content of users' posts. If users choose to post content publicly, Quizfreely displays their content to other users on quizfreely.org.
+        When users with an account take practice tests or complete review lessons, called review mode, Quizfreely collects the questions, possible answers, selected answers, accuracy, date and time, and time spent answering each question and displays these statistics to the user. These practice test and review mode statistics are NOT shared with other users.
+      </p>
+      <p class="nice-paragraph-size">
+        Quizfreely uses cookies, small files that a website stores on a user's device, ONLY to remember users' login details or settings that users choose. quizfreely.org does NOT use cookies for tracking browsing behavior or targeted advertising. quizfreely.org does NOT use any third-party cookies, cross-site cookies, or trackers.
+      </p>
+      <p class="nice-paragraph-size">
+        Data Quizfreely collects is stored to let users view/interact with their content or usage statistics. ONLY public content that users post on quizfreely.org, ONLY including public flashcard sets or studysets, study guides, and comments are analyzed to evaluate and improve Quizfreely's website, software, marketing, or promotional campains.
+      </p>
+      <p class="nice-paragraph-size">
+        Quizfreely stores collected data in databases on servers controlled by Quizfreely. Quizfreely is the ONLY data controller responsible for users' data on quizfreely.org. There are NO third parties that process users' data on quizfreely.org.
+      </p>
+      <p class="nice-paragraph-size">
+        Quizfreely does NOT disclose, share, or sell any data to any third party.
+      </p>
+      <p class="nice-paragraph-size">
+        There are NO advertisements on quizfreely.org.
       </p>
     </div>
   </div>
