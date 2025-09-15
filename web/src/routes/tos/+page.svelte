b5f2af30facd41a334b2b4d57aeb63e7e927391e
Ehan A <ehan@quizfreely.org>
2025-09-15 15:50:59 -0400

    update tos

--- web/src/routes/tos/+page.svelte
+++ web/src/routes/tos/+page.svelte
@@ -7,24 +7,24 @@
     <title>Quizfreely Terms of Service</title>
 </svelte:head>
 <main>
-  <div class="grid page">
-    <div class="content">
-      <h2>Terms of Service</h2>
-      <p class="fg0">Updated September 14, 2025 (2025-09-14)</p>
-      <p class="nice-paragraph-size">
-        Quizfreely's website, quizfreely.org, is a hosted service operated by Quizfreely ("Service"). Any use of the service is subject to these Terms and Conditions of Service ("Terms of Service") as well as <a href="/privacy">Quizfreely's Privacy Policy</a>.
-      </p>
-      <p class="nice-paragraph-size">
-        Users can post public content on quizfreely.org and can save private content under their accounts on quizfreely.org.
-        Any public content that Quizfreely's developers, maintainers, or moderators consider offensive, innaproprate, harmful, or abusive can be made private or deleted.
-        Any private content that developers, maintainers, or moderators consider harmful or abusive can be deleted.
-      </p>
-      <p class="nice-paragraph-size">
-        EXCEPT AS OTHERWISE EXPRESSLY STATED, ALL CONTENT POSTED TO OR AVAILABLE FROM THE SERVICE IS PROVIDED "AS IS", AND QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, AND ITS LICENSORS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, OR NON-INFRINGEMENT OF PROPRIETARY RIGHTS. YOU UNDERSTAND AND AGREE THAT YOU DOWNLOAD FROM, OR OTHERWISE OBTAIN CONTENT OR SERVICES THROUGH, THE SERVICE AT YOUR OWN DISCRETION AND RISK, AND THAT QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, AND ITS LICENSORS WILL HAVE NO LIABILITY OR RESPONSIBILITY FOR ANY DAMAGE TO YOUR COMPUTER SYSTEM OR DATA THAT RESULTS FROM THE DOWNLOAD SUCH CONTENT OR THE USE OF THE SERVICE.
-      </p>
-      <p class="nice-paragraph-size">
-        EXCEPT AS OTHERWISE EXPRESSLY STATED, IN NO CIRCUMSTANCE WILL QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, OR ITS LICENSORS BE LIABLE TO YOU OR ANY OTHER PARTY FOR ANY DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL OR EXEMPLARY DAMAGES, REGARDLESS OF THE BASIS OR NATURE OF THE CLAIM, RESULTING FROM ANY USE OF THE SERVICE, OR THE CONTENTS THEREOF OR OF ANY HYPERLINKED WEBSITE INCLUDING WITHOUT LIMITATION ANY LOST PROFITS, BUSINESS INTERRUPTION, LOSS OF DATA OR OTHERWISE, EVEN IF QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, OR ITS LICENSORS WERE EXPRESSLY ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.  THE PARTIES AGREE THAT THIS SECTION REPRESENTS A REASONABLE ALLOCATION OF RISK.
-      </p>
+    <div class="grid page">
+        <div class="content">
+            <h2>Terms of Service</h2>
+            <p class="fg0">Updated September 15, 2025 (2025-09-15)</p>
+            <p class="nice-paragraph-size">
+                Quizfreely's website, quizfreely.org, is a hosted service ("Service"), and any use of the Serivce is subject to the following Terms and Conditions of Service ("Terms of Service") as well as <a href="/privacy">Quizfreely's Privacy Policy</a>.
+            </p>
+            <p class="nice-paragraph-size">
+                Users can post public content on quizfreely.org and can save private content under their accounts on quizfreely.org.
+                Any public content that Quizfreely's developers, maintainers, or moderators consider offensive, innaproprate, harmful, or abusive can be made private or deleted.
+                Any private content that developers, maintainers, or moderators consider harmful or abusive can be deleted.
+            </p>
+            <p class="nice-paragraph-size">
+                EXCEPT AS OTHERWISE EXPRESSLY STATED, ALL CONTENT POSTED TO OR AVAILABLE FROM THE SERVICE IS PROVIDED "AS IS", AND QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, AND ITS LICENSORS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, OR NON-INFRINGEMENT OF PROPRIETARY RIGHTS. YOU UNDERSTAND AND AGREE THAT YOU DOWNLOAD FROM, OR OTHERWISE OBTAIN CONTENT OR SERVICES THROUGH, THE SERVICE AT YOUR OWN DISCRETION AND RISK, AND THAT QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, AND ITS LICENSORS WILL HAVE NO LIABILITY OR RESPONSIBILITY FOR ANY DAMAGE TO YOUR COMPUTER SYSTEM OR DATA THAT RESULTS FROM THE DOWNLOAD SUCH CONTENT OR THE USE OF THE SERVICE.
+            </p>
+            <p class="nice-paragraph-size">
+                EXCEPT AS OTHERWISE EXPRESSLY STATED, IN NO CIRCUMSTANCE WILL QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, OR ITS LICENSORS BE LIABLE TO YOU OR ANY OTHER PARTY FOR ANY DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL OR EXEMPLARY DAMAGES, REGARDLESS OF THE BASIS OR NATURE OF THE CLAIM, RESULTING FROM ANY USE OF THE SERVICE, OR THE CONTENTS THEREOF OR OF ANY HYPERLINKED WEBSITE INCLUDING WITHOUT LIMITATION ANY LOST PROFITS, BUSINESS INTERRUPTION, LOSS OF DATA OR OTHERWISE, EVEN IF QUIZFREELY, ITS AUTHORS, ITS SUPPLIERS, OR ITS LICENSORS WERE EXPRESSLY ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.  THE PARTIES AGREE THAT THIS SECTION REPRESENTS A REASONABLE ALLOCATION OF RISK.
+            </p>
+        </div>
     </div>
-  </div>
 </main>
