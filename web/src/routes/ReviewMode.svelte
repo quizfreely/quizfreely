52096e16699f8188b8ca2c8a57a1e6a57bbdfa4c
Ehan A <ehan@quizfreely.org>
2025-09-23 22:19:16 -0400

    work in progress

--- web/src/routes/ReviewMode.svelte
+++ web/src/routes/ReviewMode.svelte
@@ -4,6 +4,20 @@
 </script>
 <div class="grid page">
     <div class="content">
-        <h4>Overall Accuracy: </h4>
+        <button class="button-box">
+            Balanced
+        </button>
+        <button class="button-box">
+            Accuracy
+            Practice terms with the lowest accuracy
+        </button>
+        <button class="button-box">
+            Memory
+            Practice old terms that you haven't seen in a while
+        </button>
+        <button class="button-box">
+            Frequently Confused Terms
+            Practice terms that you frequently mix up
+        </button>
     </div>
 </div>
