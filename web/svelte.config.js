7413dc7215755c1368e59ef1d083e5b6555f4230
Ehan A <ehan@quizfreely.org>
2025-09-25 18:49:58 -0400

    fix env var names + make them all public unless specefied as private_

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,7 +3,11 @@ import adapter from "@sveltejs/adapter-node";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+        env: {
+            publicPrefix: "",
+            privatePrefix: "PRIVATE_"
+        }
 	}
 };
 
