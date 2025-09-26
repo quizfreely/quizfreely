e752bfa9b9faee96cec1416eb811bbb3505b7e25
Ehan A <ehan@quizfreely.org>
2025-09-26 14:50:56 -0400

    remove that from sign in load func as well

--- web/src/routes/sign-in/+page.server.js
+++ web/src/routes/sign-in/+page.server.js
@@ -1,10 +1,8 @@
 import fetchAuthData from "$lib/fetchAuthData.server";
-import { env } from "$env/dynamic/public";
 
 export async function load({ cookies }) {
     return {
         ...await fetchAuthData({ cookies }),
-        enableOAuthGoogle: (env.ENABLE_OAUTH_GOOGLE == "true"),
         header: {
             /* show sign up link button on sign in page instead of sign in link on its own page */
             showSignUpLink: true
