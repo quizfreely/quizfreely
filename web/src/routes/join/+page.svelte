767e511378e9701e081efffc6cebe06602c668d2
Ehan A <ehan@quizfreely.org>
2025-09-27 10:16:13 -0400

    make enter key enter

--- web/src/routes/join/+page.svelte
+++ web/src/routes/join/+page.svelte
@@ -7,6 +7,63 @@
     let showErrorMsg = $state(false)
     let errorMsg = $state("");
     let ws;
+    async function joinButton() {
+        showErrorMsg = false;
+        if (!codeEntered) {
+            if (gameCode.replaceAll(" ", "").length == 8) {
+                try {
+                    const respRaw = await fetch(
+                        `/realtime/status/${gameCode.replaceAll(" ", "")}`
+                    );
+                    const resp = await respRaw.json();
+                    if (resp.active) {
+                        codeEntered = true;
+                        return;
+                    } else if (resp?.active === false) {
+                        errorMsg = "Game Not Found"
+                        showErrorMsg = true;
+                        return;
+                    }
+                } catch (err) {
+                    console.error(err);
+                    errorMsg = "Server error :(\nI don't know why\nTry again mabye"
+                    showErrorMsg = true;
+                    return;
+                }
+            } else {
+                errorMsg = "Invalid Code"
+                showErrorMsg = true;
+                return;
+            }
+        } else if (uniqueName?.length > 0) {
+            ws = new WebSocket(env.REALTIME_SERVER_WS_URL+"/ws");
+            ws.onopen = () => {
+              console.log("Connected to server");
+              ws.send(JSON.stringify({ action: "join", code: gameCode.replaceAll(" ", ""), uniqueName }));
+            };
+
+            ws.onmessage = (event) => {
+                console.log("Received: " + event.data);
+                const json = JSON.parse(event.data);
+                if (json?.error && json?.msg) {
+                    errorMsg = json.msg;
+                    showErrorMsg = true;
+                }
+            };
+
+            ws.onclose = () => {
+              console.log("Connection closed");
+            };
+
+            ws.onerror = (err) => {
+              console.log("Error: " + err.message);
+            };
+        } else {
+            errorMsg = "Your name can't be blank"
+            showErrorMsg = true;
+            return;
+        }
+    }
 </script>
 <style>
     .large-textbox-thing,
@@ -32,70 +89,22 @@
                     e.target.setSelectionRange(pos, pos);
                 }
             }
+        }} onkeyup={(e) => {
+            if (e.key == "Enter") {
+                joinButton();
+            }
         }}>
         </div>
         {:else}
         <div>
-        <input class="large-textbox-thing" style="max-width: 16rem;" type="text" id="gameCode" placeholder="Name" autocomplete="off" bind:value={uniqueName} transition:scale={{duration: 400}}>
+        <input class="large-textbox-thing" style="max-width: 16rem;" type="text" id="gameCode" placeholder="Name" autocomplete="off" bind:value={uniqueName} transition:scale={{duration: 400}} onkeyup={(e) => {
+            if (e.key == "Enter") {
+                joinButton();
+            }
+        }}>
         </div>
         {/if}
-        <button class="large yay" style="width: 100%;" onclick={async () => {
-            showErrorMsg = false;
-            if (!codeEntered) {
-                if (gameCode.replaceAll(" ", "").length == 8) {
-                    try {
-                        const respRaw = await fetch(
-                            `/realtime/status/${gameCode.replaceAll(" ", "")}`
-                        );
-                        const resp = await respRaw.json();
-                        if (resp.active) {
-                            codeEntered = true;
-                            return;
-                        } else if (resp?.active === false) {
-                            errorMsg = "Game Not Found"
-                            showErrorMsg = true;
-                            return;
-                        }
-                    } catch (err) {
-                        console.error(err);
-                        errorMsg = "Server error :(\nI don't know why\nTry again mabye"
-                        showErrorMsg = true;
-                        return;
-                    }
-                } else {
-                    errorMsg = "Invalid Code"
-                    showErrorMsg = true;
-                    return;
-                }
-            } else if (uniqueName?.length > 0) {
-                ws = new WebSocket(env.REALTIME_SERVER_WS_URL+"/ws");
-                ws.onopen = () => {
-                  console.log("Connected to server");
-                  ws.send(JSON.stringify({ action: "join", code: gameCode.replaceAll(" ", ""), uniqueName }));
-                };
-
-                ws.onmessage = (event) => {
-                    console.log("Received: " + event.data);
-                    const json = JSON.parse(event.data);
-                    if (json?.error && json?.msg) {
-                        errorMsg = json.msg;
-                        showErrorMsg = true;
-                    }
-                };
-
-                ws.onclose = () => {
-                  console.log("Connection closed");
-                };
-
-                ws.onerror = (err) => {
-                  console.log("Error: " + err.message);
-                };
-            } else {
-                errorMsg = "Your name can't be blank"
-                showErrorMsg = true;
-                return;
-            }
-        }}>Join</button>
+        <button class="large yay" style="width: 100%;" onclick={joinButton}>Join</button>
         {#if showErrorMsg}
         <p class="ohno center" style="max-width: 16rem; white-space: pre-wrap;" transition:scale={{ duration: 200 }}>
             {errorMsg}
