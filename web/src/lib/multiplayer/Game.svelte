04c485e7793c4a914cd70538f92f4fa18dafedf9
Ehan A <ehan@quizfreely.org>
2025-09-30 00:45:17 -0400

    asdf

--- web/src/lib/multiplayer/Game.svelte
+++ web/src/lib/multiplayer/Game.svelte
@@ -1,5 +1,35 @@
 <script>
+    import { onMount } from "svelte";
+    let { ws, gameCode, studyset, ...props } = $props();
+    onMount(() => {
+
+    })
 </script>
-<div>
-    eee
+<div class="grid page" style="margin-top: 2rem;">
+    <div class="content">
+        <p class="fg0 center">{gameCode}</p>
+        <div class="flex" style="width: 100%; flex-direction: row;">
+            {#each playersData as playerData}
+                <div class="box grid" style="width: 100%; grid-template-rows: auto; grid-template-columns: 3fr 2fr 2fr 2fr;">
+                    <span>{playerData.uniqueName}</span>
+                    <span>{playerData.correctCount || 0}/{
+                        (playerData.incorrectCount +
+                        playerData.correctCount) || 0
+                    }</span>
+                    <span class={
+                    ((playerData.correctCount || 0) / ((
+                        playerData.incorrectCount +
+                        playerData.correctCount
+                    ) || 1)) > 0.9 ? "yay" : "ohno"
+                    }>{Math.floor((playerData.correctCount || 0) / ((
+                        playerData.incorrectCount +
+                        playerData.correctCount
+                    ) || 1) * 100)}%</span>
+                    <span class={
+                        (playerData.streak ?? 0) == 0 ? "fg0" : ""
+                    }>{playerData.streak ?? 0} streak</span>
+                </div>
+            {/each}
+        </div>
+    </div>
 </div>
--- web/src/lib/multiplayer/Leaderboard.svelte
+++ web/src/lib/multiplayer/Leaderboard.svelte
@@ -1,12 +1,41 @@
 <script>
+    import { onMount } from "svelte";
     let { ws, gameCode, ...props } = $props();
-    let players = $state(props?.players ?? []);
+    let playersData = $state(props?.players?.map(
+        player => ({
+            uniqueName: player,
+            correctCount: 10,
+            incorrectCount: 2
+        })
+    ) ?? []);
+    onMount(() => {
+
+    })
 </script>
-<div class="grid page">
+<div class="grid page" style="margin-top: 2rem;">
     <div class="content">
-        <div class="flex" style="flex-direction: row;">
-            {#each players as player}
-                {player}
+        <p class="fg0 center">{gameCode}</p>
+        <div class="flex" style="width: 100%; flex-direction: row;">
+            {#each playersData as playerData}
+                <div class="box grid" style="width: 100%; grid-template-rows: auto; grid-template-columns: 3fr 2fr 2fr 2fr;">
+                    <span>{playerData.uniqueName}</span>
+                    <span>{playerData.correctCount || 0}/{
+                        (playerData.incorrectCount +
+                        playerData.correctCount) || 0
+                    }</span>
+                    <span class={
+                    ((playerData.correctCount || 0) / ((
+                        playerData.incorrectCount +
+                        playerData.correctCount
+                    ) || 1)) > 0.9 ? "yay" : "ohno"
+                    }>{Math.floor((playerData.correctCount || 0) / ((
+                        playerData.incorrectCount +
+                        playerData.correctCount
+                    ) || 1) * 100)}%</span>
+                    <span class={
+                        (playerData.streak ?? 0) == 0 ? "fg0" : ""
+                    }>{playerData.streak ?? 0} streak</span>
+                </div>
             {/each}
         </div>
     </div>
