e150366f05b044abdf2ef68a7e93b0ea9f08cede
Ehan A <ehan@quizfreely.org>
2025-10-16 04:40:11 -0400

    hmm

--- web/src/routes/search/+page.svelte
+++ web/src/routes/search/+page.svelte
@@ -8,38 +8,38 @@
 
 <svelte:head>
     {#if data.query}
-    <title>Search "{ data.query }" - Quizfreely</title>
-    <meta name=”robots” content="noindex" />
+        <title>Search "{ data.query }" - Quizfreely</title>
+        <meta name=”robots” content="noindex" />
     {:else}
-    <title>Search Quizfreely</title>
-    <meta name="description" content="Quizfreely is a free and open source learning app with flashcards, practice tests, and more tools to help you study." />
+        <title>Search Quizfreely</title>
+        <meta name="description" content="Quizfreely is a free and open source learning app with flashcards, practice tests, and more tools to help you study." />
     {/if}
 </svelte:head>
 
 <Noscript />
 <main>
-  <div class="grid page">
-    <div class="content">
-      {#if data.query?.length >= 1 }
-        <p>Results for "{ data.query }"</p>
-        <div class="grid list" style="overflow-wrap:anywhere">
-        {#each data.results as studyset }
-        <StudysetLinkBox
-            {studyset}
-            linkTemplateFunc={id => `/studysets/${id}`}
-        ></StudysetLinkBox>
-        {:else}
-          <div class="box">
-            No results :(
-          </div>
-        {/each}
+    <div class="grid page">
+        <div class="content">
+            {#if data.query?.length >= 1 }
+                <p>Results for "{ data.query }"</p>
+                <div class="grid list" style="overflow-wrap:anywhere">
+                    {#each data.results as studyset }
+                        <StudysetLinkBox
+                            {studyset}
+                            linkTemplateFunc={id => `/studysets/${id}`}
+                        ></StudysetLinkBox>
+                    {:else}
+                        <div class="box">
+                            No results :(
+                        </div>
+                    {/each}
+                </div>
+            {:else}
+                <h2 style="text-align:center;margin-top:4rem;margin-bottom:0px">Quizfreely</h2>
+                <p class="fg0" style="text-align:center;margin-top:0.2rem">Search for resources for any topic</p>
+                <Searchbar />
+                <div style="margin-bottom:20rem"></div>
+            {/if}
         </div>
-      {:else}
-        <h2 style="text-align:center;margin-top:4rem;margin-bottom:0px">Quizfreely</h2>
-        <p class="fg0" style="text-align:center;margin-top:0.2rem">Search for resources for any topic</p>
-        <Searchbar />
-        <div style="margin-bottom:20rem"></div>
-      {/if}
     </div>
-  </div>
 </main>
