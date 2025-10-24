d43af2d8ddf3e8c97ad069574260492fbfe81fcb
Ehan A <ehan@quizfreely.org>
2025-10-24 15:58:58 -0400

    lol

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -8,9 +8,9 @@
         {#if page.status == 404}
             <p style="margin-bottom:2rem;"><a href="/">Home</a> · <a href="/search">Search</a></p>
         {/if}
-        <p>If you found a bug, error, or incorrect link, you can help fix it:</p>
+        <p>If you found an error or incorrect link, report it so we can fix it:</p>
         <p style="margin-top:0.4rem;"><a href="https://matrix.to/#/#quizfreely-space:matrix.org">Matrix space</a> · <a href="https://discord.gg/6qQrybf6kG">Discord server</a></p>
         <p style="margin-top:0.4rem;"><a href="https://codeberg.org/quizfreely/quizfreely">Codeberg</a> · <a href="https://github.com/quizfreely/quizfreely">GitHub</a></p>
-        <p class="fg0" style="margin-top:4rem;">Quizfreely is a free and open source project made by volunteers, contributors, and students. You can help by posting bug reports, feature requests, or questions.</p>
+        <p class="fg0" style="margin-top:4rem;">Quizfreely is made by students/contributors who work on this project for free. Help us improve Quizfreely by posting bug reports, feature requests, or questions.</p>
     </div>
 </div>
