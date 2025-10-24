31b3d4ee611d073522fec7a40a5a1932bdb69e66
Ehan A <ehan@quizfreely.org>
2025-10-24 17:10:06 -0400

    floating ui?

---
+++ web/src/lib/components/Dropdown.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { slide } from "svelte/transition";
+    let {
+        button,
+        buttonContent,
+        div,
+        divContent
+    } = $props();
+    let buttonElement;
+    let divElement;
+    let show = $state(false);
+</script>
+<button onclick={() => show = !show} {...button}>
+    {@render buttonContent?.()}
+</button>
+{#if show}
+<div transition:slide={{duration: 200}} {...div} class="raw-dropdown {div?.class}">
+    {@render divContent?.()}
+</div>
+{/if}
