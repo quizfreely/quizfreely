9313fb185fe70337edc567f75ead4403b10725c0
Ehan A <ehan@quizfreely.org>
2025-10-25 21:41:30 -0400

    better

--- web/src/lib/components/Searchbar.svelte
+++ web/src/lib/components/Searchbar.svelte
@@ -1,12 +1,14 @@
 <script>
     let props = $props();
+    import { tick } from "svelte";
     import { slide } from "svelte/transition";
     import IconSearch from "$lib/icons/Search.svelte";
     
-    let showAutocomplete = $state(false);
+    let doIHaveContentToShow = $state(false);
     let autocompleteItems = $state([]);
 
     let searchbarInput;
+    let wholeEntireContainer;
     let searchbarAutocomplete = $state(null);
     function onSearchbarInput(event) {
         if (event.target.value.length > 0 && event.target.value.length < 50) {
@@ -16,29 +18,56 @@
                 response.json().then(function (responseJson) {
                     if (responseJson.data && responseJson.data.queries && responseJson.data.queries.length > 0) {
                         autocompleteItems = responseJson.data.queries;
-                        showAutocomplete = true;
+                        setIfIHaveContentToShow(true);
                     } else {
-                        showAutocomplete = false;
+                        setIfIHaveContentToShow(false);
                         autocompleteItems = [];
                     }
                 }).catch(function (error) {
-                    showAutocomplete = false;
+                    setIfIHaveContentToShow(false);
                     autocompleteItems = [];
                     console.error(error);
                 })
             }).catch(function (error) {
-                showAutocomplete = false;
+                setIfIHaveContentToShow(false);
                 autocompleteItems = [];
                 console.error(error);
             })
         } else {
-            showAutocomplete = false;
+            setIfIHaveContentToShow(false);
             autocompleteItems = [];
         }
     }
+
+    function setIfIHaveContentToShow(visibility) {
+        doIHaveContentToShow = visibility;
+        updateClickHandlerAfterVisibilityChange();
+    }
+
+    function updateClickHandlerAfterVisibilityChange() {
+        tick().then(() => {
+            if (doIHaveContentToShow && !gotOutsideClicked) {
+                window.addEventListener("click", outsideClick);
+            } else {
+                window.removeEventListener("click", outsideClick);
+            }
+        });
+    }
+
+    let gotOutsideClicked = $state(false);
+    function outsideClick(e) {
+        if (!wholeEntireContainer.contains(e.target)) {
+            gotOutsideClicked = true;
+            window.removeEventListener("click", outsideClick);
+        }
+    }
+    function resetOutsideClick() {
+        gotOutsideClicked = false;
+        updateClickHandlerAfterVisibilityChange()
+    }
 </script>
 
-<form action="/search" method="get" class="searchbar with-autocomplete" style="margin-bottom: 0px; box-shadow: 0px 1px 2px 1px var(--button-box-shadow-color);">
+<form action="/search" method="get" class="searchbar with-raw-autocomplete" style="margin-bottom: 0px; box-shadow: 0px 1px 2px 1px var(--button-box-shadow-color);" bind:this={wholeEntireContainer}>
     <IconSearch class="searchbar-icon" />
     <input type="text"
         name="q"
@@ -48,12 +77,11 @@
         autocapitalize="off"
         spellcheck="false"
         value={ props.query ?? "" }
-        oninput={
-            onSearchbarInput
-        }
+        oninput={onSearchbarInput}
+        onfocus={resetOutsideClick}
     />
-    {#if showAutocomplete}
-        <div bind:this={searchbarAutocomplete} class="searchbar-autocomplete" transition:slide={{duration:200}}>
+    {#if doIHaveContentToShow && !gotOutsideClicked}
+        <div bind:this={searchbarAutocomplete} class="searchbar-raw-autocomplete" transition:slide={{duration:200}}>
             {#each autocompleteItems as item}
                 <a href="/search?q={encodeURIComponent(item?.query)}" transition:slide={{duration:200}}>
                     {item.query}
