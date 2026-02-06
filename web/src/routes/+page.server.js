56cd38f395bfa97fe8840573eb69094140a94a18
Ehan A <ehan@quizfreely.org>
2026-02-05 19:53:54 -0500

    quick fix for this little "homepage loop" UX problem

--- web/src/routes/+page.server.js
+++ web/src/routes/+page.server.js
@@ -3,7 +3,7 @@ import { load as dashboardLoad } from "./dashboard/+page.server";
 import { load as landingPageLoad } from "./landing-page/+page.server";
 
 export async function load({ cookies, locals }) {
-    if (cookies.get("dashboard") == "true") {
+    if (cookies.get("dashboard") == "true" || cookies.has("auth")) {
         redirect(307, "/dashboard");
     } else {
         return {
