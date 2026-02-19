c965685ef79cf57b1ea13e4f2e3a0c829ff74d2a
Ehan A <ehan@quizfreely.org>
2026-02-18 21:17:11 -0500

    work on user page

--- web/src/routes/users/[userid]/+page.svelte
+++ web/src/routes/users/[userid]/+page.svelte
@@ -5,6 +5,7 @@
     import UserIcon from "$lib/icons/User.svelte";
 
     let { data } = $props();
+    console.log(data);
 </script>
 
 <svelte:head>
@@ -14,24 +15,43 @@
 
 <div class="grid page">
     <div class="content">
-        <div class="flex" style="margin-top: 1rem;">
-            <UserIcon width="2rem" height="2rem" style="margin-top: 0.4rem;"
-            ></UserIcon>
-            <div
-                class="flex"
-                style="flex-direction: column; gap: 0.2rem; margin-bottom: 2rem;"
-            >
-                <h1 style="font-size: 2rem; margin-bottom: 0px;">
-                    {data.user.displayName}
-                </h1>
-                {#if data.user.username}
-                    <p class="text fg0" style="font-size: 1.2rem;">
-                        @{data.user.username}
-                    </p>
+        <div
+            class="flex"
+            style="flex-direction: row; justify-content: space-between; align-items: center;"
+        >
+            <div class="flex" style="margin-top: 1rem;">
+                <UserIcon width="2rem" height="2rem" style="margin-top: 0.4rem;"
+                ></UserIcon>
+                <div
+                    class="flex"
+                    style="flex-direction: column; gap: 0.2rem; margin-bottom: 0px;"
+                >
+                    <h1 style="font-size: 2rem; margin-bottom: 0px;">
+                        {data.user.displayName}
+                    </h1>
+                    {#if data.user.username}
+                        <p class="text fg0" style="font-size: 1.2rem;">
+                            @{data.user.username}
+                        </p>
+                    {/if}
+                </div>
+            </div>
+            <div>
+                {#if data?.user?.id == data?.authedUser?.id}
+                    <div class="flex">
+                        <a href="/settings/account" class="button alt">
+                            Account Settings
+                        </a>
+                    </div>
                 {/if}
             </div>
         </div>
 
+        {#if data?.studysetCount > 0}
+            <p class="fg0" style="text-align: end;">
+                {data?.studysetCount} public studysets
+            </p>
+        {/if}
         <div class="grid list">
             {#each data.studysets as studyset}
                 <StudysetLinkBox
