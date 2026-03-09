9c4aece95d1e05b9afebae46cc991cd56e7561f3
Ehan A <ehan@quizfreely.org>
2026-03-09 15:32:27 -0400

    typescript?

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,34 +1,34 @@
+import devtoolsJson from "vite-plugin-devtools-json";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit()
-	],
+	plugins: [sveltekit(), devtoolsJson()],
 	server: {
 		port: process?.env?.PORT ?? 8080,
 		proxy: {
 			"/api/": {
 				target: process?.env?.API_URL ?? "http://localhost:8008",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/api/, "");
 				}
 			},
 			"/realtime/": {
 				target: process?.env?.REALTIME_SERVER_URL ?? "http://localhost:8000",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/realtime/, "");
 				}
 			},
 			"/usercontent/": {
 				target: process?.env?.USERCONTENT_URL ?? "http://usercontent.web.garage.localhost:3902",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/usercontent/, "");
 				}
-			},
+			}
+
 			// "/classes/api/": {
 			// 	target: process?.env?.CLASSES_API_URL ?? "http://localhost:3000",
 			// 	changeOrigin: true,
@@ -44,24 +44,25 @@ export default defineConfig({
 			"/api/": {
 				target: process?.env?.API_URL ?? "http://localhost:8008",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/api/, "");
 				}
 			},
 			"/realtime/": {
 				target: process?.env?.REALTIME_SERVER_URL ?? "http://localhost:8000",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/realtime/, "");
 				}
 			},
 			"/usercontent/": {
 				target: process?.env?.USERCONTENT_URL ?? "http://usercontent.web.garage.localhost:3902",
 				changeOrigin: true,
-				rewrite: function (path) {
+				rewrite(path) {
 					return path.replace(/^\/usercontent/, "");
 				}
-			},
+			}
+
 			// "/classes/api/": {
 			// 	target: process?.env?.CLASSES_API_URL ?? "http://localhost:8008",
 			// 	changeOrigin: true,
