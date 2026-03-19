19c9baf93b024d5a41250a709afd605bf21370a2
Ehan A <ehan@quizfreely.org>
2026-03-18 20:02:08 -0400

    fix incorrect change from 22nd century agentic workflow

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:8008/api/graphql',
+  schema: `${process.env.API_URL ?? "http://localhost:8008"}/graphql`,
   documents: ['src/**/*.graphql'],
   generates: {
     './src/lib/graphql/generated.ts': { // Output file path
