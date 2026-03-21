612c142f38c904b8632d31c2b7e1f9c956ccef3f
Ehan A <ehan@quizfreely.org>
2026-03-21 13:18:59 -0400

    conditional config options

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices, ReporterDescription } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -18,15 +18,29 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: (() => {
+    const reporters: ReporterDescription[]  = [];
+    if (process.env.ENABLE_HTML_REPORTER != "false" /* default to true unless explicitly false */) {
+      reporters.push(["html", {
+        open: process.env.CI == "true" ? "never" : "on-failure"
+      }]);
+    }
+    if (process.env.ENABLE_GH_REPORTER == "true" /* default to false unless explicitly true */) {
+      reporters.push(["github"]);
+    }
+    return reporters;
+  })(),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.DEPLOYED ? (
+      process.env.BASE_URL ?? "https://quizfreely.org"
+    ) : `http://localhost:${process.env.PORT ?? "8080"}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,15 +53,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -71,9 +85,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: process.env.DEPLOYED == "true" ? undefined : {
+    command: 'npm run dev',
+    url: `http://localhost:${process.env.PORT ?? "8080"}`,
+    reuseExistingServer: false,
+  },
 });
