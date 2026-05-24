6e2cea4d44e9b2a5d28ae373da976802189412dc
Ehan A <ehan@quizfreely.org>
2026-05-24 15:42:04 -0400

    fix

--- src/routes/import/scholarsome/+page.svelte
+++ src/routes/import/scholarsome/+page.svelte
@@ -281,9 +281,9 @@
             <ScholarsomeLogotype width="auto" height="3.6rem" role="img" aria-label="Scholarsome"></ScholarsomeLogotype>
         </div>
         <div class="flex" style="flex-direction: column; align-items: center;">
-            <div class="flex" style="flex-direction: column; align-items: stretch;">
+            <div class="flex" style="flex-direction: column; align-items: stretch; width: 26rem;">
                 <p style="font-size: 1.2rem; margin-top: 2rem; text-align: start;">Paste a link below</p>
-                <div class="searchbar" style="min-width: 24rem; max-width: 30rem; margin-bottom: 0px; box-shadow: 0px 1px 2px 1px var(--button-box-shadow-color);">
+                <div class="searchbar" style="width: 100%; margin-bottom: 0px; box-shadow: 0px 1px 2px 1px var(--button-box-shadow-color);">
                     <LinkIcon class="searchbar-icon"></LinkIcon>
                     <input type="text" placeholder="https://scholarsome.com/study-set/..." autocomplete="off" autocapitalize="off" spellcheck="false" bind:value={link} bind:this={textbox}>
                 </div>
