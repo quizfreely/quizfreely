0c205c12bcb36673829dd06d9d51b47588b49a8f
Ehan A <ehan@quizfreely.org>
2026-05-25 19:02:34 -0400

    bugfix and ui update

--- src/routes/studysets/[id]/match/+page.svelte
+++ src/routes/studysets/[id]/match/+page.svelte
@@ -2,5 +2,6 @@
     import Match from "../../../Match.svelte";
 
     let { data } = $props();
+    console.log("logged data", data)
 </script>
 <Match local={false} cloudId={data.cloudId} {data}></Match>
