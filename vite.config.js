895b00a3acd640d2b7a52ce94a307383ac40c16d
Ehan A <ehan@quizfreely.org>
2026-06-06 15:32:50 -0400

    fix env variables not being used, add medama proxy

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,73 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-const proxy = {
-    "/api/": {
-        target: process?.env?.API_URL ?? "http://localhost:8008",
-        changeOrigin: true,
-        rewrite: function (path) {
-            return path.replace(/^\/api/, "");
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const proxy = {
+        "/api/": {
+            target: env?.API_URL ?? "http://localhost:8008",
+            changeOrigin: true,
+            rewrite: function (path) {
+                return path.replace(/^\/api/, "");
+            },
+            configure: (proxy, _options) => {
+                proxy.on("proxyRes", (proxyRes, req, res) => {
+                    const setCookie = proxyRes.headers['set-cookie'];
+                    if (setCookie) {
+                        const cookies = Array.isArray(setCookie) ? setCookie : [setCookie];
+                            
+                        proxyRes.headers["set-cookie"] = cookies.map(
+                            cookie => cookie.replace(/;\s*Secure/gi, "")
+                        );
+                    }
+                });
+            }
         },
-        configure: (proxy, _options) => {
-            proxy.on("proxyRes", (proxyRes, req, res) => {
-                const setCookie = proxyRes.headers['set-cookie'];
-                if (setCookie) {
-                    const cookies = Array.isArray(setCookie) ? setCookie : [setCookie];
-                        
-                    proxyRes.headers["set-cookie"] = cookies.map(
-                        cookie => cookie.replace(/;\s*Secure/gi, "")
-                    );
+        "/realtime/": {
+            target: env?.REALTIME_SERVER_URL ?? "http://localhost:8000",
+            changeOrigin: true,
+            rewrite: function (path) {
+                return path.replace(/^\/realtime/, "");
+            }
+        },
+        "/usercontent/": {
+            target: env?.USERCONTENT_URL ?? "http://usercontent.web.garage.localhost:3902",
+            changeOrigin: true,
+            rewrite: function (path) {
+                return path.replace(/^\/usercontent/, "");
+            }
+        },
+        ...(env?.ENABLE_MEDAMA == "true" && env?.MEDAMA_PROXY_TARGET_URL?.length > 0 ? {
+            "/medama/": {
+                target: env?.MEDAMA_PROXY_TARGET_URL,
+                changeOrigin: true,
+                rewrite: function (path) {
+                    return path.replace(/^\/medama/, "");
                 }
-            });
-        }
-    },
-    "/realtime/": {
-        target: process?.env?.REALTIME_SERVER_URL ?? "http://localhost:8000",
-        changeOrigin: true,
-        rewrite: function (path) {
-            return path.replace(/^\/realtime/, "");
-        }
-    },
-    "/usercontent/": {
-        target: process?.env?.USERCONTENT_URL ?? "http://usercontent.web.garage.localhost:3902",
-        changeOrigin: true,
-        rewrite: function (path) {
-            return path.replace(/^\/usercontent/, "");
-        }
-    },
-    // "/classes/api/": {
-    // 	target: process?.env?.CLASSES_API_URL ?? "http://localhost:3000",
-    // 	changeOrigin: true,
-    // 	rewrite: function (path) {
-    // 		return path.replace(/^\/classes\/api/, "");
-    // 	}
-    // }
-};
+            }
+        } : {})
+        // "/classes/api/": {
+        // 	target: env?.CLASSES_API_URL ?? "http://localhost:3000",
+        // 	changeOrigin: true,
+        // 	rewrite: function (path) {
+        // 		return path.replace(/^\/classes\/api/, "");
+        // 	}
+        // }
+    };
 
-export default defineConfig({
-	plugins: [
-		sveltekit()
-	],
-	server: {
-		port: process?.env?.PORT ?? 8080,
-        proxy
-	},
-	preview: {
-		port: process?.env?.PORT ?? 8080,
-		proxy
-    }
+	return {
+        plugins: [
+	    	sveltekit()
+	    ],
+	    server: {
+	    	port: process?.env?.PORT ?? 8080,
+            proxy
+	    },
+	    preview: {
+	    	port: process?.env?.PORT ?? 8080,
+	    	proxy
+        }
+    };
 });
