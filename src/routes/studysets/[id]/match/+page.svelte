c888a76699d22477c3a30dd9fe629282009d2cae
Ehan A <ehan@quizfreely.org>
2026-06-16 11:52:33 +0000

    remove match before merging to prod for now

--- src/routes/studysets/[id]/match/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Match from "../../../Match.svelte";
-
-    let { data } = $props();
-    console.log("logged data", data)
-</script>
-<Match local={false} cloudId={data.cloudId} {data}></Match>
