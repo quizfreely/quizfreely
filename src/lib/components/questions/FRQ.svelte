74de7e757dab4dd4b0c8de358cf5cba96f6fe76a
Ehan A <ehan@quizfreely.org>
2026-06-27 20:46:05 -0400

    wow

--- src/lib/components/questions/FRQ.svelte
+++ src/lib/components/questions/FRQ.svelte
@@ -1,7 +1,7 @@
 <script>
     import CheckmarkIcon from "$lib/icons/Checkmark.svelte";
     import XMarkIcon from "$lib/icons/CloseXMark.svelte";
-    let { term, answerWith, viewOnly, showAccuracy, answerUpdateCallback, userMarkedCorrectCallback } = $props();
+    let { term, answerWith, viewOnly, showAccuracy, answerUpdateCallback, showCorrectAnswer } = $props();
     let manuallyMarkedCorrect = $state(false);
     export function getQuestion() {
         if (answer == "") {
@@ -16,10 +16,10 @@
                     def: term.def
                 },
                 answerWith: answerWith,
-                correct: manuallyMarkedCorrect ||
-                    (answerWith == "DEF" ?
-                        term.def == answer:
-                        term.term == answer),
+                correct: manuallyMarkedCorrect || (answerWith == "DEF" ?
+                    term.def.trim().toLowerCase() == answer.trim().toLowerCase() :
+                    term.term.trim().toLowerCase() == answer.trim().toLowerCase()
+                ),
                 userMarkedCorrect: manuallyMarkedCorrect,
                 answeredString: answer,
             }
@@ -35,7 +35,10 @@
     <p class="h4" style="white-space: pre-wrap; margin-bottom: 0px;">{ answerWith == "DEF" ?
         term.term : term.def
     }</p>
-    <div class="flex" style="align-content: center; align-items: center;">
+    {#if showCorrectAnswer}
+    <p class="fg0">You entered:</p>
+    {/if}
+    <div class="flex" style="align-content: center; align-items: center; {showCorrectAnswer ? "margin-top: 0.4rem;" : ""}">
         <input type="text" placeholder="{answerWith == "DEF" ?
             "Definition" : "Term"
         }" bind:value={answer} style="min-width: 16rem; field-sizing: content;" class="{showAccuracy ? (
@@ -43,17 +46,40 @@
                 "yay" : "ohno"
         ) : ""}" disabled={viewOnly} oninput={answerUpdateCallback}>
         {#if showAccuracy && getQuestion().frq.correct}
-            <span class="yay"><CheckmarkIcon width="1.4rem" height="1.4rem"></CheckmarkIcon></span>
+            <span class="yay"><CheckmarkIcon width="1.6rem" height="1.6rem"></CheckmarkIcon></span>
         {:else if showAccuracy}
-            <span class="ohno"><XMarkIcon width="1.4rem" height="1.4rem"></XMarkIcon></span>
-        {/if}
-        {#if showAccuracy && !getQuestion().frq.correct}
-            <div>
-            <button class="faint" onclick={() => {
-                manuallyMarkedCorrect = true;
-                userMarkedCorrectCallback()
-            }}>Manually mark as correct?</button>
-            </div>
+            <span class="ohno"><XMarkIcon width="1.6rem" height="1.6rem"></XMarkIcon></span>
         {/if}
     </div>
+    {#if showAccuracy && showCorrectAnswer}
+        <p class="fg0">Correct { answerWith == "DEF" ? "definition" : "term"}:</p>
+        <p class={(answerWith == "DEF" ? term.def : term.term)?.length < 20 ? "h4" : ""} style="white-space: pre-wrap; margin-bottom: 0px; margin-top: 0.2rem;">{ answerWith == "DEF" ?
+            term.def : term.term
+        }</p>
+    {/if}
+    {#if showAccuracy && manuallyMarkedCorrect}
+        <div class="flex" style="align-items: center;">
+            <p class="fg0">Manually marked correct</p>
+            <button class="warn alt" onclick={() => {
+                manuallyMarkedCorrect = false;
+            }}>Undo?</button>
+        </div>
+    {:else if showAccuracy && showCorrectAnswer && !getQuestion().frq.correct}
+        <button class="warn alt" onclick={() => {
+            manuallyMarkedCorrect = true;
+        }}>Manually mark as correct?</button>
+    {/if}
+    {#if showAccuracy && showCorrectAnswer}
+        <div>
+        <button class="faint" onclick={() => {
+            showCorrectAnswer = false;
+        }}>Hide Correct Answer</button>
+        </div>
+    {:else if showAccuracy && (manuallyMarkedCorrect || !getQuestion().frq.correct)}
+        <div>
+        <button class="faint" onclick={() => {
+            showCorrectAnswer = true;
+        }}>Show Correct Answer</button>
+        </div>
+    {/if}
 </div>
