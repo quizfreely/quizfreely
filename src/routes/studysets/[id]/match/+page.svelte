d534b9a22193efc34b92091691a2fa23a0fc7984
Ehan A <ehan@quizfreely.org>
2026-07-01 14:19:50 -0400

    dont need that logging anymore

--- src/routes/studysets/[id]/match/+page.svelte
+++ src/routes/studysets/[id]/match/+page.svelte
@@ -2,6 +2,5 @@
     import Match from "../../../Match.svelte";
 
     let { data } = $props();
-    console.log("logged data", data)
 </script>
 <Match local={false} cloudId={data.cloudId} {data}></Match>
