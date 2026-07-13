6d006dbe3a355ebf6efad242f00ecc4851c6bcad
Ehan A <ehan@quizfreely.org>
2026-07-12 22:28:26 -0400

    icons

---
+++ src/lib/icons/No.svelte
@@ -0,0 +1,4 @@
+<script>
+    let { width = "1rem", height = "1rem", ...props } = $props();
+</script>
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 512 512" {...props} class={props.class}><path d="M367.2 412.5 99.5 144.8C77.1 176.2 64 214.6 64 256c0 106 86 192 192 192 41.5 0 79.9-13.1 111.2-35.5m45.3-45.3C434.9 335.8 448 297.4 448 256c0-106-86-192-192-192-41.5 0-79.9 13.1-111.2 35.5zM0 256a256 256 0 1 1 512 0 256 256 0 1 1-512 0" fill="currentColor"/></svg>
