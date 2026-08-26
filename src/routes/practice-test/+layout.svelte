1fc3e20b3b98695e459da0c8415f20f8457b0326
Ehan A <ehan@quizfreely.org>
2026-08-26 14:01:38 -0400

    Make sure PT layout based nav doesn't loose trans

--- src/routes/practice-test/+layout.svelte
+++ src/routes/practice-test/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
     import { page } from "$app/state";
+    import { fade } from "svelte/transition";
+    import { sineIn, sineOut } from "svelte/easing";
     let { children } = $props();
     let r = $derived(page.url.searchParams.get("r") ?? 0)
 </script>
 {#key r}
-    {@render children()}
+    <div in:fade={{ duration: 120, delay: 120, easing: sineIn }} out:fade={{ duration: 120, easing: sineOut }}>
+        {@render children()}
+    </div>
 {/key}
