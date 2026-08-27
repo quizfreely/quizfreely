8c6000ea0700f5deafa94f70333fdc7664d11de3
Ehan A <ehan@quizfreely.org>
2026-08-27 14:09:16 -0400

    Rewrite term stats load func

--- src/routes/term-stats/+page.js
+++ src/routes/term-stats/+page.js
@@ -1,65 +1,89 @@
-export async function load({ params, fetch }) {
+export async function load({ fetch, url }) {
+    const cloudTermId = url.searchParams.get("term");
+    let localTermId = url.searchParams.get("term");
+    if (localTermId != null && !isNaN(localTermId)) {
+        localTermId = Number(localTermId);
+    }
+    const cloudStudysetIds = url.searchParams.getAll("studyset");
+    const localStudysetIds = url.searchParams.getAll("localStudyset").map(Number).filter((n) => !Number.isNaN(n));
+    let data;
     try {
+        let variables;
+        let query = `{
+            authed
+            authedUser {
+                id
+                username
+                displayName
+            }
+        }`;
+        if (cloudTermId) {
+            variables = {
+                cloudTermId,
+            };
+            query = `query termStats($cloudTermId: ID!) {
+                authed
+                authedUser {
+                    id
+                    username
+                    displayName
+                }
+                term(id: $cloudTermId) {
+                    id
+                    term
+                    def
+                    termImageUrl
+                    defImageUrl
+                    progress {
+                        termFirstReviewedAt
+                        termLastReviewedAt
+                        termReviewCount
+                        defFirstReviewedAt
+                        defLastReviewedAt
+                        defReviewCount
+                        termCorrectCount
+                        termIncorrectCount
+                        defCorrectCount
+                        defIncorrectCount
+                    }
+                    reviewEventStatsByDay(last: 30) {
+                        timestamp
+                        correct
+                        incorrect
+                    }
+                }
+            }`;
+        }
         const respRaw = await fetch("/api/graphql", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                query: `query termStats($termId: ID!) {
-    authed
-    authedUser {
-        id
-        username
-        displayName
-    }
-    term(id: $termId) {
-        id
-        term
-        def
-        termImageUrl
-        defImageUrl
-        progress {
-            termFirstReviewedAt
-            termLastReviewedAt
-            termReviewCount
-            defFirstReviewedAt
-            defLastReviewedAt
-            defReviewCount
-            termCorrectCount
-            termIncorrectCount
-            defCorrectCount
-            defIncorrectCount
-        }
-        reviewEventStatsByDay(last: 30) {
-            timestamp
-            correct
-            incorrect
-        }
-    }
-}`,
-                variables: {
-                    termId: params.termid
-                }
-            })
+                query,
+                variables,
+            }),
         });
         const resp = await respRaw.json();
         if (resp?.data == null || resp?.errors != null) {
-            console.log("Error in cloud term stats load func api request. Response: ", resp);
+            console.log("Error in term stats load func api request. Response: ", resp);
         }
-        return {
-            studysetId: params.id,
-            termId: params.termid,
+        data = {
             term: resp?.data?.term,
             authed: resp?.data?.authed,
             authedUser: resp?.data?.authedUser,
-        }
+        };
     } catch (err) {
-        console.error("Error in cloud term stats load func: ", err);
-        return {
-          studysetId: params.id,
-          termId: params.termid,
-          authed: false,
-        }
+        console.error("Error in term stats load func: ", err);
+        data = {
+            authed: false,
+        };
     }
+    return {
+        ...data,
+        cloudTermId,
+        localTermId,
+        cloudStudysetIds,
+        localStudysetIds,
+    };
 }
