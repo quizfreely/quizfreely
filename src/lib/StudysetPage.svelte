8cfe270995e98b659ebfca59b0bb9579b9ba183f
Ehan A <ehan@quizfreely.org>
2026-08-29 15:31:38 -0400

    abstract terms list/table

--- src/lib/StudysetPage.svelte
+++ src/lib/StudysetPage.svelte
@@ -6,6 +6,7 @@
     import { fade, slide } from "svelte/transition";
     let { data } = $props();
 
+    import TermsTable from "$lib/components/TermsTable.svelte";
     import Flashcards from "$lib/components/Flashcards.svelte";
     import Dropdown from "$lib/components/Dropdown.svelte";
     import FolderPicker from "$lib/components/FolderPicker.svelte";
@@ -388,32 +389,7 @@
                         </a>
                     </div>
 
-                    <table class="outer caption box qzfr-terms-list">
-                        <tbody>
-                            <tr>
-                                <th>Term</th>
-                                <th>Definition</th>
-                            </tr>
-                            {#if terms != null}
-                                {#each terms as term}
-                                    <tr>
-                                        <td style="vertical-align: top; padding: 0px;">
-                                            <div style="white-space: pre-wrap; overflow-wrap: break-word; padding-left: 1rem; padding-right: 1rem; padding-top: 1rem; padding-bottom: 0px;">{term.term}</div>
-                                            {#if term?.termImageUrl != null}
-                                                <div><img src={term.termImageUrl} alt="term image" class="term-image"></div>
-                                            {/if}
-                                        </td>
-                                        <td style="vertical-align: top; padding: 0px;">
-                                            <div style="white-space: pre-wrap; overflow-wrap: break-word; padding-left: 1rem; padding-right: 1rem; padding-top: 1rem; padding-bottom: 0px;">{term.def}</div>
-                                            {#if term?.defImageUrl != null}
-                                                <div style="padding-left: 0.6rem;"><img src={term.defImageUrl} alt="definition image" class="term-image"></div>
-                                            {/if}
-                                        </td>
-                                    </tr>
-                                {/each}
-                            {/if}
-                        </tbody>
-                    </table>
+                    <TermsTable {terms} class="caption" />
                 </div>
                 {#if showDeleteConfirmationModal}
                     <div class="modal" transition:fade={{ duration: 200 }}>
@@ -759,16 +735,3 @@
         </div>
     </div>
 </main>
-
-<style>
-    .term-image {
-        max-width: 18.6rem;
-        max-height: 300px;
-        margin: 0px;
-        padding: 0px;
-        border-radius: 0.8rem;
-    }
-    .qzfr-terms-list {
-        max-width: 90vw;
-    }
-</style>
--- src/routes/combine/+page.svelte
+++ src/routes/combine/+page.svelte
@@ -1,26 +1,37 @@
 <script>
     import { studysetSelection } from "$lib/studyset-selection.svelte.js";
     import { onMount } from "svelte";
+    import { idbApiLayer } from "$lib/idb-api-layer";
+    import TermsTable from "$lib/components/TermsTable.svelte";
     import StudysetLinkBox from "$lib/components/StudysetLinkBox.svelte";
     import GridIcon from "$lib/icons/AppsGrid.svelte";
     import GraphIcon from "$lib/icons/ChartGraphLine.svelte";
     import PTIcon from "$lib/icons/PracticeTestChecklist.svelte";
     import TrashIcon from "$lib/icons/Trash.svelte";
+    import AngleUpIcon from "$lib/icons/AngleUp.svelte"
+    import AngleDownIcon from "$lib/icons/AngleDown.svelte"
     let { data } = $props();
     let currentCloudIds = [...data.cloudIds];
     let currentLocalIds = [...data.localIds];
     let studysets = $state(data.studysets?.filter?.(s => s != null) ?? []);
+    let collapsed = $state(true);
+    let displayedStudysets = $derived(collapsed ? studysets.slice(0, 6) : studysets);
     let terms = $derived(studysets.flatMap(s => s?.terms).filter(t => t != null));
     onMount(() => {
         studysetSelection.clearSelection();
+        (async () => {
+            if (currentLocalIds.length > 0) {
+                studysets.push(...(await idbApiLayer.getStudysetsByIds(currentLocalIds))?.filter?.(s => s != null) ?? []);
+            }
+        })();
     });
 </script>
 <div class="grid page" style="padding-top: 2rem;">
     <div class="content">
         <p class="h4" style="margin-bottom: 0px; font-size: 1.8rem;">{currentCloudIds.length + currentLocalIds.length} {currentCloudIds.length + currentLocalIds.length == 1 ? "Studyset" : "Studysets"}</p>
         <p class="fg0" style="margin-top: 0.2rem; font-size: 1.2rem;">{terms.length} {terms.length == 1 ? "Total Term" : "Total Terms"}</p>
-        <div class="grid list">
-        {#each studysets as studyset, index}
+        <div class="grid list" style="margin-bottom: 0px;">
+        {#each displayedStudysets as studyset, index}
             <StudysetLinkBox {studyset} linkTemplateFunc={
                 (id) => id?.includes?.("-") ?
                     `/studysets/${id}` : `/studyset/local?id=${id}`
@@ -35,6 +46,15 @@
                 {/snippet}
             </StudysetLinkBox>
         {/each}
+        </div>
+        <div class="flex center" style="margin-top: 0.6rem;">
+            <button class="faint" onclick={() => collapsed = !collapsed}>
+                {#if collapsed}
+                    <AngleDownIcon /> Show All
+                {:else}
+                    <AngleUpIcon /> Collapse
+                {/if}
+            </button>
         </div>
         <div class="grid list">
             <a href={`/match?${[
@@ -59,5 +79,6 @@
                 Progress &amp; Stats
             </a>
         </div>
+        <TermsTable {terms} />
     </div>
 </div>
