fe8b27cf93d51ab555163fcd95a94da08dc2569e
Ehan A <ehan@quizfreely.org>
2026-08-30 13:02:51 -0400

    multi select UI for folders

--- src/routes/folder/[id]/+page.svelte
+++ src/routes/folder/[id]/+page.svelte
@@ -3,6 +3,7 @@
     import { slide, fade, scale } from "svelte/transition";
     import { sineIn, sineOut } from "svelte/easing";
     import { goto } from "$app/navigation";
+    import { studysetSelection } from "$lib/studyset-selection.svelte.js";
     import Dropdown from "$lib/components/Dropdown.svelte";
     import FolderPicker from "$lib/components/FolderPicker.svelte";
     import StudysetLinkBox from "$lib/components/StudysetLinkBox.svelte";
@@ -17,6 +18,8 @@
     import MoreIcon from "$lib/icons/MoreDotsVertical.svelte";
     import IconEyeSlash from "$lib/icons/EyeSlash.svelte";
     import IconEye from "$lib/icons/Eye.svelte";
+    import GridIcon from "$lib/icons/AppsGrid.svelte";
+    import CheckmarkSquareIcon from "$lib/icons/CheckmarkSquare.svelte";
 
     let { data } = $props();
 
@@ -294,6 +297,8 @@
             window.removeEventListener("click", windowOnclick);
         };
     });
+    
+    let selectingMultiple = $state(false);
 </script>
 
 <svelte:head>
@@ -336,43 +341,93 @@
         </button>
     </div>
     {/if}
-    <div class="flex" style="align-items: center; margin-top: 1rem;">
-        <FolderIcon width="2rem" height="2rem"></FolderIcon>
-        <p class="h3" style="margin-bottom: 0px;">
-            {folder?.name}
-        </p>
+    {const amAuthor = $derived(data?.authedUser?.id != null && data?.authedUser?.id == data?.folder?.user?.id)}
+    <div class="flex" style="align-items: center; justify-content: space-between; margin-top: 1rem;">
+        <div>
+            <div class="flex" style="align-items: center;">
+                <FolderIcon width="2rem" height="2rem"></FolderIcon>
+                <p class="h3" style="margin-bottom: 0px;">
+                    {folder?.name}
+                </p>
+            </div>
+            {#if !amAuthor}
+            <p style="margin-top: 0.4rem;">Created by <a href="/users/{data?.folder?.user?.id}">{
+                data?.folder?.user?.displayName
+            }</a></p>
+            {/if}
+        </div>
+        {#if amAuthor}
+        <div class="flex" style="align-items: center;">
+            <button class="alt" onclick={showFolderRenaming}>
+                <PencilIcon />
+                Rename Folder
+            </button>
+            <Dropdown
+                button={{ class: "dropdown-toggle" }}
+                placement="bottom-end"
+            >
+                {#snippet buttonContent()}
+                    <MoreIcon></MoreIcon>
+                {/snippet}
+                {#snippet divContent()}
+                    <button
+                        class="ohno"
+                        onclick={showDeleteFolderConfirmation}
+                    >
+                        <TrashIcon></TrashIcon> Delete Folder
+                    </button>
+                {/snippet}
+            </Dropdown>
+        </div>
+        {/if}
     </div>
-    {#if data?.authedUser?.id != null && data?.authedUser?.id == data?.folder?.user?.id}
-    <div class="flex" style="align-items: center;">
-        <button onclick={newStudysetButton}>
-            <IconPlus />
-            New Studyset
+    {#snippet multiselect()}
+        <button class="alt" onclick={() => {
+            selectingMultiple = true;
+            studysets?.forEach?.(s => {
+                if (s?.id != null) {
+                    studysetSelection.select(
+                        s.id?.includes?.("-") ? {
+                            cloudId: s.id,
+                        } : {
+                            localId: s.id,
+                        },
+                    );
+                }
+            });
+        }}>
+            <CheckmarkSquareIcon />
+            Select All
         </button>
-        <button class="alt" onclick={showFolderRenaming}>
-            <PencilIcon />
-            Rename Folder
+        <button onclick={() => selectingMultiple = !selectingMultiple} class="alt {selectingMultiple ? "text fg1" : ""}">
+            {#if selectingMultiple}
+                <GridIcon></GridIcon>
+                Stop Selecting
+            {:else}
+                <GridIcon></GridIcon>
+                Select Multiple
+            {/if}
         </button>
-        <Dropdown
-            button={{ class: "dropdown-toggle" }}
-            placement="bottom-end"
-        >
-            {#snippet buttonContent()}
-                <MoreIcon></MoreIcon>
-            {/snippet}
-            {#snippet divContent()}
-                <button
-                    class="ohno"
-                    onclick={showDeleteFolderConfirmation}
-                >
-                    <TrashIcon></TrashIcon> Delete Folder
-                </button>
-            {/snippet}
-        </Dropdown>
+    {/snippet}
+    {#if amAuthor}
+    <div class="flex" style="align-items: center; justify-content: space-between; column-gap: 2rem; row-gap: 1rem; margin-top: 2rem;">
+        <div class="flex" style="align-items: center;">
+            <button onclick={newStudysetButton}>
+                <IconPlus />
+                New Studyset
+            </button>
+        </div>
+        <div class="flex" style="align-items: center;">
+            {@render multiselect()}
+        </div>
     </div>
     {:else}
-        <p>Created by <a href="/users/{data?.folder?.user?.id}">{
-            data?.folder?.user?.displayName
-        }</a></p>
+        <div class="flex" style="align-items: center; justify-content: space-between; column-gap: 2rem; row-gap: 1rem;">
+            <span class="fg0" style="font-size: 1.1rem;">{studysets?.length ?? 0} {studysets?.length == 1 ? "Studyset" : "Studysets"}</span>
+            <div class="flex" style="align-items: center;">
+                {@render multiselect()}
+            </div>
+        </div>
     {/if}
     {#if studysets?.length > 0}
         <div class="grid list" style="overflow-wrap: anywhere;">
@@ -382,6 +437,16 @@
                     linkTemplateFunc={(id) => `/studysets/${id}`}
                     showDropdown={data.authed}
                     dropdownContent={studysetDropdown}
+                    button={selectingMultiple}
+                    buttonOnClick={(_event, studyset) => {
+                        studysetSelection.toggleSelect(
+                            studyset.id?.includes?.("-") ? {
+                                cloudId: studyset.id,
+                            } : {
+                                localId: studyset.id,
+                            },
+                        );
+                    }}
                 ></StudysetLinkBox>
             {/each}
         </div>
