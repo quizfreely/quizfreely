66a7abe6fbac9297f5e8a6005a8aaafa192fe762
Ehan A <ehan@quizfreely.org>
2026-08-31 14:03:25 -0400

    so annoying

--- src/routes/lab/+page.svelte
+++ src/routes/lab/+page.svelte
@@ -6,39 +6,81 @@
     const chartDataSrc = $state([
         {
             id: "23e",
-            title: "abc title",
+            title: "Englisch Unit 4 und 5",
             accuracy: 1,
         },
         {
             id: "dfs290",
-            title: "title goes here",
+            title: "Psychology Ch1",
             accuracy: 0.2,
         },
         {
             id: "1b3e",
-            title: "ABCD TITLE!",
+            title: "Untitled Studyset",
             accuracy: 1,
         },
         {
             id: "1b33e",
-            title: "ABCDEF TITLE!",
-            accuracy: 1,
+            title: "Wordly Wise 3000 Level 8, Lesson 15",
+            accuracy: 0.8,
+        },
+        {
+            id: "1b33e",
+            title: "Wordly Wise 3000 Level 8, Lesson 8",
+            accuracy: 0.8,
+        },
+        {
+            id: "1b33e",
+            title: "body cavities and serous membranes",
+            accuracy: 0.8,
+        },
+        {
+            id: "1b33e",
+            title: "anatomical planes and sections",
+            accuracy: 0.8,
+        },
+        {
+            id: "1b33e",
+            title: "CAM V@ FInancial Terms and Formulas",
+            accuracy: 0.8,
         },
     ]);
     const chartData = $derived.by(() => {
         const newChartData = [];
-        chartDataSrc.forEach((d, index) => {
+        /* loop to clone array with new objects because we can't reference the same reactive objects */
+        chartDataSrc.forEach(d => {
             newChartData.push({
-                ...d,
-                index
+                id: d.id,
+                title: d.title,
+                accuracy: d.accuracy,
+                titleShort: d.title?.length > 30 ? `${d.title?.slice(0, 30)}...` : d.title,
             });
+        });
+        newChartData.sort((a, b) => b.accuracy - a.accuracy);
+        /* loop again after sorting because index is updated */
+        newChartData.forEach((d, index) => {
+            d.index = index;
         });
         return newChartData;
     });
 </script>
+<style>
+.grid.qzfr-combined-studyset-stats-grid {
+    gap: 1rem;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto;
+}
+</style>
 
 <div class="grid page">
     <div class="content">
+{const height = $derived.by(() => {
+    const v = 200 + 50*(chartData.length - 2);
+    console.log(v);
+    return v;
+})}
+<div class="grid qzfr-combined-studyset-stats-grid">
+<div style="min-height: {height}px;">
 <Chart
 	data={chartData}
 	y="index"
@@ -46,12 +88,12 @@
 	x="accuracy"
 	xDomain={[0, null]}
 	xNice
-	padding={{ left: 24, bottom: 20, top: 8 }}
-	height={300}
+	padding={{ left: 100, top: 10, bottom: 20, right: 20 }}
+	height={height}
 	tooltipContext={{ mode: 'band' }}
 >
 	<Layer>
-		<Axis placement="left" rule format={(index) => chartData[index].title} />
+		<Axis placement="left" rule format={(index) => chartData[index].titleShort}/>
 		<Axis placement="bottom" grid rule format={(v) => `${Math.round(v*100)}%`} />
         {#each chartData as d, i}
             <Bar
@@ -82,5 +124,15 @@
 			{/snippet}
 		</Tooltip.Root>
 </Chart>
+</div>
+            <div class="box flex" style="flex-direction: column;">
+                {#each chartData as d}
+                    <span>{d.title}</span>
+                {/each}
+            </div>
+</div>
+        <div class="box">
+Next box
+            </div>
 </div>
 </div>
