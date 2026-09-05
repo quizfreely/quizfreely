b4319ae8452ea076de6a4c3873df5d974f083439
Ehan A <ehan@quizfreely.org>
2026-09-05 19:29:18 -0400

    add sticky subheader for search page

--- src/routes/search/+page.js
+++ src/routes/search/+page.js
@@ -60,7 +60,10 @@ export async function load({ url, fetch }) {
           pageInfo: searchConn?.pageInfo,
           PER_PAGE,
           authed: authed,
-          authedUser: authedUser
+          authedUser: authedUser,
+          studysetSelection: {
+            allowStickySubHeader: true
+          }
         }
       } else {
         console.log("Error in search +page.js, api res: ", apiRes)
